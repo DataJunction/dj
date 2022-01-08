9de91cbc41194aa68101793afd2912a337822ef7
Beto Dealmeida <roberto@dealmeida.net>
2022-01-07 16:58:29 -0800

    feat: DAG builder for nodes

--- src/datajunction/cli/compile.py
+++ src/datajunction/cli/compile.py
@@ -12,11 +12,13 @@ This will:
 
 import asyncio
 import logging
+import re
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Set
 
 import yaml
+from rich.text import Text
 from sqlalchemy import inspect
 from sqlmodel import Session, create_engine, select
 
@@ -27,17 +29,20 @@ from datajunction.models import (
     Representation,
     get_name_from_path,
 )
-from datajunction.utils import create_db_and_tables, get_session
+from datajunction.utils import create_db_and_tables, get_session, render_dag
 
 _logger = logging.getLogger(__name__)
 
 
-def load_data(path: Path) -> Dict[str, Any]:
+async def load_data(repository: Path, path: Path) -> Dict[str, Any]:
     """
     Load data from a YAML file.
     """
     with open(path, encoding="utf-8") as input_:
         data = yaml.safe_load(input_)
+
+    data["name"] = get_name_from_path(repository, path)
+    data["path"] = path
 
     return data
 
@@ -111,10 +116,9 @@ async def index_databases(repository: Path, session: Session) -> List[Database]:
             created_at = None
 
         _logger.info("Loading database from config %s", path)
-        data = load_data(path)
+        data = await load_data(repository, path)
 
         _logger.info("Creating database %s", name)
-        data["name"] = name
         data["created_at"] = created_at or datetime.now(timezone.utc)
         data["updated_at"] = datetime.now(timezone.utc)
         database = Database(**data)
@@ -161,71 +165,133 @@ def get_columns(representations: List[Representation]) -> List[Column]:
     return list(columns.values())
 
 
-async def index_nodes(repository: Path, session: Session) -> List[Node]:
+def get_dependencies(expression: str) -> Set[str]:
+    """
+    Return all the dependencies from a SQL expression.
+
+    This should be done with a SQL parser instead.
+    """
+    pattern = re.compile("FROM (.*)")
+    match = pattern.search(expression)
+    return {match.group(1)} if match else set()
+
+
+async def index_nodes(  # pylint: disable=too-many-locals
+    repository: Path,
+    session: Session,
+) -> List[Node]:
     """
     Index all the nodes, computing their schema.
+
+    We first compute the schema of source nodes, since they are simply fetched from the
+    database using SQLAlchemy. After that we compute the schema of downstream nodes, as
+    the schema of source nodes become available.
     """
+    directory = repository / "nodes"
+
     # load all databases
     databases = {
         database.name: database for database in session.exec(select(Database)).all()
     }
 
-    directory = repository / "nodes"
+    # load all nodes and their dependencies
+    tasks = [load_data(repository, path) for path in directory.glob("**/*.yaml")]
+    configs = await asyncio.gather(*tasks)
 
-    async def add_from_path(path: Path) -> Node:
-        name = get_name_from_path(repository, path)
-        _logger.info("Processing node %s", name)
-
-        # check if the node was already indexed and if it's up-to-date
-        query = select(Node).where(Node.name == name)
-        node = session.exec(query).one_or_none()
-        if node:
-            # compare file modification time with timestamp on DB
-            mtime = path.stat().st_mtime
-
-            # some DBs like SQLite will drop the timezone info; in that case
-            # we assume it's UTC
-            if node.updated_at.tzinfo is None:
-                node.updated_at = node.updated_at.replace(tzinfo=timezone.utc)
-
-            if node.updated_at > datetime.fromtimestamp(mtime, tz=timezone.utc):
-                _logger.info("Node %s is up-do-date, skipping", name)
-                return node
-
-            # delete existing node
-            created_at = node.created_at
-            session.delete(node)
-            session.flush()
+    dependencies: Dict[str, Set[str]] = {}
+    for config in configs:
+        if "expression" in config:
+            dependencies[config["name"]] = get_dependencies(config["expression"])
         else:
-            created_at = None
+            dependencies[config["name"]] = set()
+    _logger.info("DAG:\n%s", Text.from_ansi(render_dag(dependencies)))
 
-        _logger.info("Loading node from config %s", path)
-        data = load_data(path)
+    # compute the schema of nodes with upstream nodes already indexed
+    nodes: List[Node] = []
+    started: Set[str] = set()
+    finished: Set[str] = set()
+    pending_tasks: Set[asyncio.Task] = set()
+    while True:
+        to_process = [
+            config
+            for config in configs
+            if dependencies[config["name"]] <= finished
+            and config["name"] not in started
+        ]
+        if not to_process and not pending_tasks:
+            break
+        started |= {config["name"] for config in to_process}
+        new_tasks = {
+            add_node(session, databases, config["path"], config)
+            for config in to_process
+        }
 
-        # create representations and columns
-        representations = []
-        for database_name, representation_data in data["representations"].items():
-            representation_data["database"] = databases[database_name]
-            representation = Representation(**representation_data)
-            representations.append(representation)
-        data["representations"] = representations
-        data["columns"] = get_columns(representations)
-
-        _logger.info("Creating node %s", name)
-        data["name"] = name
-        data["created_at"] = created_at or datetime.now(timezone.utc)
-        data["updated_at"] = datetime.now(timezone.utc)
-        node = Node(**data)
-
-        session.add(node)
-        session.flush()
-
-        return node
-
-    tasks = [add_from_path(path) for path in directory.glob("**/*.yaml")]
-    nodes = await asyncio.gather(*tasks)
+        done, pending_tasks = await asyncio.wait(
+            pending_tasks | new_tasks,
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+        for future in done:
+            node = future.result()
+            nodes.append(node)
+            finished.add(node.name)
 
     return nodes
+
+
+async def add_node(
+    session: Session,
+    databases: Dict[str, Database],
+    path: Path,
+    data: Dict[str, Any],
+) -> Node:
+    """
+    Index a node given its YAML config.
+    """
+    name = data["name"]
+    _logger.info("Processing node %s", name)
+
+    # check if the node was already indexed and if it's up-to-date
+    query = select(Node).where(Node.name == name)
+    node = session.exec(query).one_or_none()
+    if node:
+        # compare file modification time with timestamp on DB
+        mtime = path.stat().st_mtime
+
+        # some DBs like SQLite will drop the timezone info; in that case
+        # we assume it's UTC
+        if node.updated_at.tzinfo is None:
+            node.updated_at = node.updated_at.replace(tzinfo=timezone.utc)
+
+        if node.updated_at > datetime.fromtimestamp(mtime, tz=timezone.utc):
+            _logger.info("Node %s is up-do-date, skipping", name)
+            return node
+
+        # delete existing node
+        created_at = node.created_at
+        session.delete(node)
+        session.flush()
+    else:
+        created_at = None
+
+    # create representations and columns
+    representations = []
+    for database_name, representation_data in data.get("representations", {}).items():
+        representation_data["database"] = databases[database_name]
+        representation = Representation(**representation_data)
+        representations.append(representation)
+    data["representations"] = representations
+    data["columns"] = get_columns(representations)
+
+    _logger.info("Creating node %s", name)
+    data["name"] = name
+    data["created_at"] = created_at or datetime.now(timezone.utc)
+    data["updated_at"] = datetime.now(timezone.utc)
+    node = Node(**data)
+
+    session.add(node)
+    session.flush()
+
+    return node
 
 
 async def run(repository: Path) -> None:
