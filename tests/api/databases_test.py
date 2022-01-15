0beb080f264324b017788b8adb5a43e18f568d12
Beto Dealmeida <roberto@dealmeida.net>
2022-01-14 19:09:00 -0800

    feat: split API files

---
+++ tests/api/databases_test.py
@@ -0,0 +1,33 @@
+"""
+Tests for the FastAPI application.
+"""
+
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from datajunction.models import Database
+
+
+def test_read_databases(session: Session, client: TestClient) -> None:
+    """
+    Test ``GET /databases/``.
+    """
+    database = Database(
+        name="gsheets",
+        description="A Google Sheets connector",
+        URI="gsheets://",
+        read_only=True,
+    )
+    session.add(database)
+    session.commit()
+
+    response = client.get("/databases/")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 1
+    assert data[0]["name"] == "gsheets"
+    assert data[0]["URI"] == "gsheets://"
+    assert data[0]["description"] == "A Google Sheets connector"
+    assert data[0]["read_only"] is True
