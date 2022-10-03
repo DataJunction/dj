0aa32b5d10dbab8228f4e15cbb2f53248c3be6d9
Nick Ouellet <quebecname@gmail.com>
2022-10-02 21:46:22 -0400

    some gql tests

---
+++ tests/api/graphql/query_test.py
@@ -0,0 +1,27 @@
+from sqlmodel import Session
+from fastapi.testclient import TestClient
+
+from datajunction.models.database import Database
+
+def test_get_databases(session: Session, client: TestClient):
+    db1 = Database(id= 1, name="db1", URI="")
+    db2 = Database(id= 2, name="db2", URI="")
+  
+
+    session.add(db1)
+    session.add(db2)
+    session.commit()
+
+    query="""
+    {
+        getDatabases{
+            id,
+            name
+        }
+    }
+    """
+
+
+    response = client.post("/graphql", json={'query': query})
+    assert response.json() == {'data': {'getDatabases': [{'id': 1, 'name': 'db1'}, {'id': 2, 'name': 'db2'}]}}
+    
