994bec1b4ead6a06ce9da9d96d602b73f715d127
samredai <43911210+samredai@users.noreply.github.com>
2023-01-31 00:52:40 -0500

    Add a POST /nodes/validate/ route

--- tests/api/nodes_test.py
+++ tests/api/nodes_test.py
@@ -1,7 +1,7 @@
 """
 Tests for the nodes API.
 """
-
+import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
@@ -61,3 +61,249 @@ def test_read_nodes(session: Session, client: TestClient) -> None:
             "type": "INT",
         },
     ]
+
+
+class TestValidateNodes:  # pylint: disable=too-many-public-methods
+    """
+    Test ``POST /nodes/validate/``.
+    """
+
+    @pytest.fixture
+    def session(self, session: Session) -> Session:
+        """
+        Add nodes to facilitate testing of the validation route
+        """
+
+        node1 = Node(
+            name="revenue_source",
+            type=NodeType.SOURCE,
+            columns=[
+                Column(name="payment_id", type=ColumnType.INT),
+                Column(name="payment_amount", type=ColumnType.FLOAT),
+                Column(name="customer_id", type=ColumnType.INT),
+                Column(name="account_type", type=ColumnType.STR),
+            ],
+        )
+        node2 = Node(
+            name="large_revenue_payments_only",
+            query=(
+                "SELECT payment_id, payment_amount, customer_id, account_type "
+                "FROM revenue_source WHERE payment_amount > 1000000"
+            ),
+            type=NodeType.TRANSFORM,
+            columns=[
+                Column(name="payment_id", type=ColumnType.INT),
+                Column(name="payment_amount", type=ColumnType.FLOAT),
+                Column(name="customer_id", type=ColumnType.INT),
+                Column(name="account_type", type=ColumnType.STR),
+            ],
+        )
+        node3 = Node(
+            name="large_revenue_payments_and_business_only",
+            query=(
+                "SELECT payment_id, payment_amount, customer_id, account_type "
+                "FROM revenue_source WHERE payment_amount > 1000000 "
+                "AND account_type = 'BUSINESS'"
+            ),
+            type=NodeType.TRANSFORM,
+            columns=[
+                Column(name="payment_id", type=ColumnType.INT),
+                Column(name="payment_amount", type=ColumnType.FLOAT),
+                Column(name="customer_id", type=ColumnType.INT),
+                Column(name="account_type", type=ColumnType.STR),
+            ],
+        )
+        session.add(node1)
+        session.add(node2)
+        session.add(node3)
+        session.commit()
+        return session
+
+    def test_validating_a_valid_node(self, client: TestClient) -> None:
+        """
+        Test validating a valid node
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo transform node!",
+                "query": "SELECT payment_id FROM large_revenue_payments_only",
+                "type": "transform",
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 200
+        assert len(data) == 5
+        assert data["columns"] == [
+            {
+                "dimension_column": None,
+                "dimension_id": None,
+                "id": None,
+                "name": "payment_id",
+                "type": "INT",
+            },
+        ]
+        assert data["status"] == "valid"
+        assert data["node"]["status"] == "valid"
+        assert data["dependencies"][0]["name"] == "large_revenue_payments_only"
+        assert data["message"] == "Node `foo` is valid"
+        assert data["node"]["id"] is None
+        assert data["node"]["mode"] == "published"
+        assert data["node"]["name"] == "foo"
+        assert (
+            data["node"]["query"]
+            == "SELECT payment_id FROM large_revenue_payments_only"
+        )
+        assert data["node"]["type"] == "transform"
+
+    def test_validating_an_invalid_node(self, client: TestClient) -> None:
+        """
+        Test validating an invalid node
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo transform node!",
+                "query": "SELECT bar FROM large_revenue_payments_only",
+                "type": "transform",
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 500
+        assert data == {
+            "message": "Cannot resolve type of column bar.",
+            "errors": [],
+            "warnings": [],
+        }
+
+    def test_validating_invalid_sql(self, client: TestClient) -> None:
+        """
+        Test validating an invalid node with invalid SQL
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo transform node!",
+                "query": "SUPER invalid SQL query",
+                "type": "transform",
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 500
+        assert data == {
+            "errors": [],
+            "message": "Query parsing failed.\n"
+            "\tsql parser error: Expected an SQL statement, found: SUPER",
+            "warnings": [],
+        }
+
+    def test_validating_with_missing_parents(self, client: TestClient) -> None:
+        """
+        Test validating a node with a query that has missing parents
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo transform node!",
+                "query": "SELECT 1 FROM node_that_does_not_exist",
+                "type": "transform",
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 500
+        assert data == {
+            "message": "Node definition contains references to nodes that do not exist",
+            "errors": [
+                {
+                    "code": 201,
+                    "message": "Node definition contains references to nodes that do not exist",
+                    "debug": {"missing_parents": ["node_that_does_not_exist"]},
+                    "context": "",
+                },
+            ],
+            "warnings": [],
+        }
+
+    def test_allowing_missing_parents_for_draft_nodes(self, client: TestClient) -> None:
+        """
+        Test validating a draft node that's allowed to have missing parents
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo transform node!",
+                "query": "SELECT 1 FROM node_that_does_not_exist",
+                "type": "transform",
+                "mode": "draft",
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["message"] == "Node `foo` is valid"
+        assert data["status"] == "valid"
+        assert data["node"]["name"] == "foo"
+        assert data["node"]["mode"] == "draft"
+        assert data["node"]["status"] == "valid"
+        assert data["columns"] == [
+            {
+                "id": None,
+                "name": "_col0",
+                "type": "INT",
+                "dimension_id": None,
+                "dimension_column": None,
+            },
+        ]
+
+    def test_raise_when_trying_to_validate_a_source_node(
+        self,
+        client: TestClient,
+    ) -> None:
+        """
+        Test validating a source node which is not possible
+        """
+
+        response = client.post(
+            "/nodes/validate/",
+            json={
+                "name": "foo",
+                "description": "This is my foo source node!",
+                "type": "source",
+                "columns": [
+                    {"name": "payment_id", "type": "INT"},
+                    {"name": "payment_amount", "type": "FLOAT"},
+                    {"name": "customer_id", "type": "INT"},
+                    {"name": "account_type", "type": "INT"},
+                ],
+                "tables": [
+                    {
+                        "database_id": 1,
+                        "catalog": "test",
+                        "schema": "accounting",
+                        "table": "revenue",
+                    },
+                ],
+            },
+        )
+        data = response.json()
+
+        assert response.status_code == 500
+        assert data == {
+            "message": "Source nodes cannot be validated",
+            "errors": [],
+            "warnings": [],
+        }
