d2351272b57d5ec7cc7f503867a49ccb12013fa2
Yian Shang <yshang@netflix.com>
2023-02-11 00:30:05 -0800

    Add endpoint to list all downstream nodes from a given node

--- tests/cli/urls_test.py
+++ tests/cli/urls_test.py
@@ -37,6 +37,7 @@ http://localhost:8000/nodes/{name}/revisions/: List all revisions for the node.
 http://localhost:8000/nodes/{name}/columns/{column}/: Add information to a node column
 http://localhost:8000/nodes/{name}/table/: Add a table to a node
 http://localhost:8000/nodes/similarity/{node1_name}/{node2_name}: Compare two nodes by how similar their queries are
+http://localhost:8000/nodes/{name}/downstream/: List all nodes that are downstream from the given node, filterable by type.
 http://localhost:8000/data/availability/{node_name}/: Add an availability state to a node
 http://localhost:8000/health/: Healthcheck for services.
 http://localhost:8000/graphql: GraphQL endpoint.
