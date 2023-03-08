2293ab772adeea7e57dcfb549c28754cca6e1198
Nick Ouellet <nick@ouellet.dev>
2023-03-08 13:02:17 -0500

    Update build to assume common catalog and find availability states

--- dj/models/catalog.py
+++ dj/models/catalog.py
@@ -16,8 +16,7 @@ from dj.models.engine import Engine, EngineInfo
 from dj.utils import UTCDatetime
 
 if TYPE_CHECKING:
-    from dj.models import Table
-    from dj.models import NodeRevision
+    from dj.models import NodeRevision, Table
 
 
 class CatalogEngines(BaseSQLModel, table=True):  # type: ignore
