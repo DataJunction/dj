79dda39fa3c1720acfc7a35e91e931e2af5ab92e
Yian Shang <yian.shang@gmail.com>
2023-04-06 16:47:10 -0700

    Add setting for CORS whitelist

--- dj/api/main.py
+++ dj/api/main.py
@@ -52,7 +52,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=settings.cors_origin_whitelist,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
