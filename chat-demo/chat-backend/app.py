4d0a92d190c183f338cb384da8a9be46c429a58b
Nick Ouellet <35903677+CircArgs@users.noreply.github.com>
2023-04-08 23:26:19 -0400

    Chat (#10)
    * streamlit chat example
    
    * whoops fixing dropped code
    
    * add screencap
    
    * screencap mp4
    
    * Add files via upload
    
    * Add files via upload
    
    * remove junk
    
    * redo
    
    * remove api key
    
    * failsafes and sql requests. using json schemas
    
    * format sql in ui as code

--- chat-demo/chat-backend/app.py
+++ chat-demo/chat-backend/app.py
@@ -70,24 +70,40 @@ def make_request(utterance: str, max_retries: int = 5, model: str ='gpt-3.5-turb
 
 app = FastAPI()
 
-schema = '''
-    {
-        "metric": required string of single metric name chosen,
-        "groupbys": [array of string of columns chosen],
-        "filters": [array of string of sql expressions using columns chosen],
-    }
-'''
 
 @app.get("/query/{query}")
 def query(query: str, n: int = 5, rel:float = 0):
     metric_query_prompt = f"""
-    From the query '{query}', determine what the metric being calculated is. Reply only in the form: Metric: [the metric from the query]
+    From the query '{query}', determine what the metric being calculated is.
+    If the query wants sql, then 'data' is false otherwise it is true. 
+    If the query is irrelevant to data or sql queries, then 'valid' is false otherwise it is true.
+    A valid query is one that requests a metric which is some aggregation of data usually numeric.
+    
+    Respond with only json in the schema below and no additional commentary:
+    
+    {{
+        metric: string of the metric from the query e.g. "average cost of food",
+        data: boolean indicating whether the query is requesting data or sql code,
+        valid: boolean indicating whether the query is a legitimate query
+    }}
     """.strip()
 
     metric_query = make_request(metric_query_prompt)
+    logging.info(f"Metric query schema response: ", metric_query)
+    metric_query_json = json.loads(metric_query)
+    
+    metric_query=metric_query_json['metric']
+    data_flag=metric_query_json['data']
+    valid=metric_query_json['valid']
+    
+    if not valid:
+        return {"message": "Your query does not appear to be about data or sql. Please try again."}
 
-    metric_query = metric_query.replace("Metric:", "").strip()
     relevant_metrics = query_node(metric_query, n)
+
+    if not len(relevant_metrics):
+        return {"message": "No metrics that match your query were found."}
+    
     metric_dimensions=("\n".join(relevant_metrics['name']+" ("+relevant_metrics['description']+"): "+relevant_metrics['dimensions'].astype(str)))
     request_prompt = f"""
     Your task is to give what columns to group by and what columns to filter by to answer the question '{query}'. 
@@ -99,21 +115,33 @@ def query(query: str, n: int = 5, rel:float = 0):
 
     You may choose only a single metric.
     Be sure to only use columns from the list for your chosen metric in the dimension and filter query parameters.
+    If you do not believe any of the choices are relevant, leave the 'metric' field as an empty string.
 
     Respond with only json in the schema below and no additional commentary:
 
-    {schema}
+    {{
+        "metric": required string of single metric name chosen,
+        "groupbys": [array of string of columns chosen],
+        "filters": [array of string of sql expressions using columns chosen],
+    }}
     """.strip()
     
     request_info = make_request(request_prompt)
-    logging.info(f"Schema response: ", request_info)
+    logging.info(f"Metric info schema response: ", request_info)
     request_json = json.loads(request_info)
 
-    metric=request_json['metric']
+    metric=request_json['metric'].strip()
+    if not metric:
+        return {"message": "No metrics that match your query were found."}
+
+    # if metric not in  metrics['name']:
+    #     return {"message": "We could not determine an appropriate metric from your query."} 
     dimensions=",".join(dim.strip() for dim in request_json['groupbys'])
     filters=",".join(dim.strip() for dim in request_json['filters'])
-    
-    request_url=f"{DJ_URL}/data/{metric}/?dimensions={dimensions}&filters={filters}"
+    if not (dimensions or filters):
+        return {"message": "It appears you do not want any filters or aggregations. Please do so."}
+    base_route = 'data' if data_flag else 'sql'
+    request_url=f"{DJ_URL}/{base_route}/{metric}/?dimensions={dimensions}&filters={filters}"
     
     logging.info(f"Querying URL: {request_url}")
     
