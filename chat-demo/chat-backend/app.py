5334b4099f48fec49e375a176ebeefc65eecb53a
Samuel Redai <sredai@netflix.com>
2023-04-08 23:29:40 -0400

    fix linters (#11)

--- chat-demo/chat-backend/app.py
+++ chat-demo/chat-backend/app.py
@@ -33,9 +33,9 @@ metric_embeddings = embedder.encode(metrics.description, convert_to_tensor=True)
 logging.info("Loaded metric query model and embedded data")
 
 def query_node(query: str, top_k = 5):
-    """given a query for a dimension or metric name, queries for the nearest names 
+    """given a query for a dimension or metric name, queries for the nearest names
     and returns the columns.
-    
+
     Returns:
     {metric_or_dimension_name: [columns]}
     """
@@ -66,7 +66,7 @@ def make_request(utterance: str, max_retries: int = 5, model: str ='gpt-3.5-turb
     if retries == max_retries:
         logging.error("Maximum number of retries exceeded. Giving up.")
         sys.exit(1)
-        
+
 
 app = FastAPI()
 
@@ -75,12 +75,12 @@ app = FastAPI()
 def query(query: str, n: int = 5, rel:float = 0):
     metric_query_prompt = f"""
     From the query '{query}', determine what the metric being calculated is.
-    If the query wants sql, then 'data' is false otherwise it is true. 
+    If the query wants sql, then 'data' is false otherwise it is true.
     If the query is irrelevant to data or sql queries, then 'valid' is false otherwise it is true.
     A valid query is one that requests a metric which is some aggregation of data usually numeric.
-    
+
     Respond with only json in the schema below and no additional commentary:
-    
+
     {{
         metric: string of the metric from the query e.g. "average cost of food",
         data: boolean indicating whether the query is requesting data or sql code,
@@ -91,11 +91,11 @@ def query(query: str, n: int = 5, rel:float = 0):
     metric_query = make_request(metric_query_prompt)
     logging.info(f"Metric query schema response: ", metric_query)
     metric_query_json = json.loads(metric_query)
-    
+
     metric_query=metric_query_json['metric']
     data_flag=metric_query_json['data']
     valid=metric_query_json['valid']
-    
+
     if not valid:
         return {"message": "Your query does not appear to be about data or sql. Please try again."}
 
@@ -103,12 +103,12 @@ def query(query: str, n: int = 5, rel:float = 0):
 
     if not len(relevant_metrics):
         return {"message": "No metrics that match your query were found."}
-    
+
     metric_dimensions=("\n".join(relevant_metrics['name']+" ("+relevant_metrics['description']+"): "+relevant_metrics['dimensions'].astype(str)))
     request_prompt = f"""
-    Your task is to give what columns to group by and what columns to filter by to answer the question '{query}'. 
-    `dimensions` are the names of dimensiones comma-separated. 
-    `filters` are comma-separated filters like in a sql query using dimension names e.g. `dimension.col=something`: 
+    Your task is to give what columns to group by and what columns to filter by to answer the question '{query}'.
+    `dimensions` are the names of dimensiones comma-separated.
+    `filters` are comma-separated filters like in a sql query using dimension names e.g. `dimension.col=something`:
 
     Here is a list of "metric name (metric description): [columns]":
     {metric_dimensions}
@@ -125,7 +125,7 @@ def query(query: str, n: int = 5, rel:float = 0):
         "filters": [array of string of sql expressions using columns chosen],
     }}
     """.strip()
-    
+
     request_info = make_request(request_prompt)
     logging.info(f"Metric info schema response: ", request_info)
     request_json = json.loads(request_info)
@@ -135,16 +135,16 @@ def query(query: str, n: int = 5, rel:float = 0):
         return {"message": "No metrics that match your query were found."}
 
     # if metric not in  metrics['name']:
-    #     return {"message": "We could not determine an appropriate metric from your query."} 
+    #     return {"message": "We could not determine an appropriate metric from your query."}
     dimensions=",".join(dim.strip() for dim in request_json['groupbys'])
     filters=",".join(dim.strip() for dim in request_json['filters'])
     if not (dimensions or filters):
         return {"message": "It appears you do not want any filters or aggregations. Please do so."}
     base_route = 'data' if data_flag else 'sql'
     request_url=f"{DJ_URL}/{base_route}/{metric}/?dimensions={dimensions}&filters={filters}"
-    
+
     logging.info(f"Querying URL: {request_url}")
-    
+
     metrics_data = requests.get(request_url)
     return metrics_data.json()
 
