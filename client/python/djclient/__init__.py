48a5e2af0fccbae8983fd242527d20552e9811e6
Yian Shang <yian.shang@gmail.com>
2023-04-10 18:12:12 -0700

    Adding imports to __all__ and updating README

--- client/python/djclient/__init__.py
+++ client/python/djclient/__init__.py
@@ -2,3 +2,17 @@
 A DataJunction client for connecting to a DataJunction server
 """
 __version__ = "0.0.1a1"
+
+from djclient.dj import DJClient, Source, Catalog, Dimension, Transform, Metric, Cube, Node, Engine
+
+__all__ = [
+    "DJClient",
+    "Source",
+    "Catalog",
+    "Dimension",
+    "Transform",
+    "Metric",
+    "Cube",
+    "Node",
+    "Engine",
+]
