8eefc02fd5cf8e6d35cf98122896ced82496e386
samredai <sredai@netflix.com>
2023-04-10 20:47:22 -0400

    update engine api function names

--- dj/api/engines.py
+++ dj/api/engines.py
@@ -16,7 +16,7 @@ router = APIRouter()
 
 
 @router.get("/engines/", response_model=List[EngineInfo])
-def get_an_engine(*, session: Session = Depends(get_session)) -> List[EngineInfo]:
+def list_engines(*, session: Session = Depends(get_session)) -> List[EngineInfo]:
     """
     List all available engines
     """
@@ -24,7 +24,7 @@ def get_an_engine(*, session: Session = Depends(get_session)) -> List[EngineInfo
 
 
 @router.get("/engines/{name}/{version}/", response_model=EngineInfo)
-def get_engine_metadata(
+def get_an_engine(
     name: str, version: str, *, session: Session = Depends(get_session)
 ) -> EngineInfo:
     """
