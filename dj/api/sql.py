b4381d6e7205c9c58db6b1e6ee2ccb09f6a54836
Yian Shang <yian.shang@gmail.com>
2023-04-28 07:43:50 -0700

    Add endpoint that exposes sql generation for multiple metrics

--- dj/api/sql.py
+++ dj/api/sql.py
@@ -8,7 +8,8 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session
 
-from dj.api.helpers import get_engine, get_query
+from dj.api.helpers import get_engine, get_query, validate_cube
+from dj.construction.build import build_metric_nodes
 from dj.models.metric import TranslatedSQL
 from dj.models.query import ColumnMetadata
 from dj.utils import get_session
@@ -41,6 +42,46 @@ def get_sql(
         dimensions=dimensions,
         filters=filters,
         engine=engine,
+    )
+    columns = [
+        ColumnMetadata(name=col.alias_or_name.name, type=str(col.type))  # type: ignore
+        for col in query_ast.select.projection
+    ]
+    return TranslatedSQL(
+        sql=str(query_ast),
+        columns=columns,
+        dialect=engine.dialect if engine else None,
+    )
+
+
+@router.get("/sql/", response_model=TranslatedSQL)
+def get_sql_for_metrics(
+    metrics: List[str] = Query([]),
+    dimensions: List[str] = Query([]),
+    filters: List[str] = Query([]),
+    *,
+    session: Session = Depends(get_session),
+    engine_name: Optional[str] = None,
+    engine_version: Optional[str] = None,
+) -> TranslatedSQL:
+    """
+    Return SQL for a set of metrics with dimensions and filters
+    """
+    engine = (
+        get_engine(session, engine_name, engine_version)  # type: ignore
+        if engine_name
+        else None
+    )
+    _, metric_nodes, _, _ = validate_cube(
+        session,
+        metrics,
+        dimensions,
+    )
+    query_ast = build_metric_nodes(
+        session,
+        metric_nodes,
+        filters=filters or [],
+        dimensions=dimensions or [],
     )
     columns = [
         ColumnMetadata(name=col.alias_or_name.name, type=str(col.type))  # type: ignore
