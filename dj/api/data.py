cd2abae9716fdc2d8fdc03c83bbd875736bd1d9f
Samuel Redai <sredai@netflix.com>
2023-05-02 23:38:06 -0400

    /data/ endpoint for multiple metrics and dimensions (#486)

--- dj/api/data.py
+++ dj/api/data.py
@@ -9,7 +9,8 @@ from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
 from sqlmodel import Session
 
-from dj.api.helpers import get_engine, get_node_by_name, get_query
+from dj.api.helpers import get_engine, get_node_by_name, get_query, validate_cube
+from dj.construction.build import build_metric_nodes
 from dj.errors import DJException, DJInvalidInputException
 from dj.models.metric import TranslatedSQL
 from dj.models.node import AvailabilityState, AvailabilityStateBase, NodeType
@@ -137,6 +138,69 @@ def get_data(  # pylint: disable=too-many-locals
     query_create = QueryCreate(
         engine_name=engine.name,
         catalog_name=node.current.catalog.name,
+        engine_version=engine.version,
+        submitted_query=query.sql,
+        async_=async_,
+    )
+    result = query_service_client.submit_query(query_create)
+    # Inject column info if there are results
+    if result.results.__root__:  # pragma: no cover
+        result.results.__root__[0].columns = columns
+    return result
+
+
+@router.get("/data/", response_model=QueryWithResults)
+def get_data_for_metrics(  # pylint: disable=R0914
+    metrics: List[str] = Query([]),
+    dimensions: List[str] = Query([]),
+    filters: List[str] = Query([]),
+    async_: bool = False,
+    *,
+    session: Session = Depends(get_session),
+    query_service_client: QueryServiceClient = Depends(get_query_service_client),
+    engine_name: Optional[str] = None,
+    engine_version: Optional[str] = None,
+) -> QueryWithResults:
+    """
+    Return data for a set of metrics with dimensions and filters
+    """
+    leading_metric_node = get_node_by_name(session, metrics[0])
+    available_engines = leading_metric_node.current.catalog.engines
+    engine = (
+        get_engine(session, engine_name, engine_version)  # type: ignore
+        if engine_name
+        else available_engines[0]
+    )
+    if engine not in available_engines:
+        raise DJInvalidInputException(  # pragma: no cover
+            f"The selected engine is not available for the node {metrics[0]}. "
+            f"Available engines include: {', '.join(engine.name for engine in available_engines)}",
+        )
+
+    _, metric_nodes, _, _ = validate_cube(
+        session,
+        metrics,
+        dimensions,
+    )
+    query_ast = build_metric_nodes(
+        session,
+        metric_nodes,
+        filters=filters or [],
+        dimensions=dimensions or [],
+    )
+    columns = [
+        ColumnMetadata(name=col.alias_or_name.name, type=str(col.type))  # type: ignore
+        for col in query_ast.select.projection
+    ]
+    query = TranslatedSQL(
+        sql=str(query_ast),
+        columns=columns,
+        dialect=engine.dialect if engine else None,
+    )
+
+    query_create = QueryCreate(
+        engine_name=engine.name,
+        catalog_name=leading_metric_node.current.catalog.name,
         engine_version=engine.version,
         submitted_query=query.sql,
         async_=async_,
