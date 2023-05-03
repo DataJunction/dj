8b643991a7b88fe868715f859f393482a826a5f6
Yian Shang <yian.shang@gmail.com>
2023-05-03 07:14:16 -0700

    Fixing build and linters

--- src/app/components/Tab.jsx
+++ src/app/components/Tab.jsx
@@ -7,15 +7,8 @@ export default class Tab extends Component {
       <div className={selectedTab === id ? 'col active' : 'col'}>
         <div className="header-tabs nav-overflow nav nav-tabs">
           <div className="nav-item">
-            <button
-              id={id}
-              role="button"
-              className="nav-link"
-              tabIndex="0"
-              onClick={onClick}
-            >
+            <button id={id} className="nav-link" tabIndex="0" onClick={onClick}>
               {this.props.name}
-              {/*<span className="rounded-pill badge bg-secondary-soft">823</span>*/}
             </button>
           </div>
         </div>
