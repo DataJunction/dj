34a0eb0591719aa97b4e206379d6aa2b2fe9a31b
Samuel Redai <sredai@netflix.com>
2023-05-02 21:24:28 -0400

    npm installable javascript DJ client (#484)
    * npm installable javascript DJ client
    
    * remove test.html file
    
    * add javascript linters and build workflow
    
    * fix docs
    
    * Set javascript steps working-directory
    
    * add a step to install babel
    
    * npm install dev dependencies
    
    * working-dir
    
    * lint the github workflow definition
    
    * create a UMD hybrid module using webpack
    
    * no need to set lodash as external, not using
    
    * Set version to 0.0.1-rc.0
    
    * Change docs example to list metrics

---
+++ client/javascript/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'datajunction.js',
+    globalObject: 'this',
+    library: {
+      name: 'datajunction',
+      type: 'umd',
+    },
+  },
+};
