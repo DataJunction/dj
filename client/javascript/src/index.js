786c1379a84fb76a7e08908636dd30783fd3ce4e
samredai <sredai@netflix.com>
2023-05-05 02:57:25 -0400

    Add more methods for js client

--- client/javascript/src/index.js
+++ client/javascript/src/index.js
@@ -1,51 +1,157 @@
 import HttpClient from './httpclient.js'
 
 export class DJClient extends HttpClient {
-  constructor (baseURL) {
+  constructor (baseURL, namespace, engineName = null, engineVersion = null) {
     super({
       baseURL
     })
+    this.namespace = namespace
+    this.engineName = engineName
+    this.engineVersion = engineVersion
   }
 
   get catalogs () {
     return {
-      get: () => this.get('/catalogs/')
+      list: () => this.get('/catalogs/'),
+      get: (catalog) => this.get(`/catalogs/${catalog}/`),
+      create: (catalog) => this.get('/catalogs/', catalog)
     }
   }
 
   get engines () {
     return {
-      get: () => this.get('/engines/')
+      list: () => this.get('/engines/'),
+      get: (engineName, engineVersion) => this.get(`/engines/${engineName}/${engineVersion}/`),
+      create: (engine) => this.post('/engines/', engine)
+    }
+  }
+
+  get attachEngineToCatalog () {
+    return {
+      get: (engineName, engineVersion) => this.get(`/engines/${engineName}/${engineVersion}/`),
+      create: (engine) => this.post('/engines/', engine)
     }
   }
 
   get namespaces () {
     return {
-      get: () => this.get('/namespaces/')
+      list: () => this.get('/namespaces/'),
+      nodes: (namespace) => this.get(`/namespaces/${namespace}/`),
+      create: (namespace) => this.post(`/namespaces/${namespace}/`)
     }
   }
 
-  get metrics () {
+  get commonDimensions () {
     return {
-      get: () => this.get('/metrics/')
+      list: (metrics) => this.get(`/metrics/common/dimensions/?metrics=${metrics}`)
     }
   }
 
   get nodes () {
     return {
-      get: () => this.get('/nodes/')
+      get: (name) => this.get(`/nodes/${name}/`),
+      validate: (node) => this.post('/nodes/validate/', node),
+      update: (name, node) => this.patch(`/nodes/${name}/`, node),
+      revisions: (name) => this.get(`/nodes/${name}/revisions/`),
+      downstream: (name) => this.get(`/nodes/${name}/downstream/`),
+      upstream: (name) => this.get(`/nodes/${name}/upstream/`)
+    }
+  }
+
+  get sources () {
+    return {
+      create: (source) => this.post('/nodes/source/', source),
+      list: () => this.get(`/namespaces/${this.namespace}/?type_=source`)
+    }
+  }
+
+  get transforms () {
+    return {
+      create: (transform) => this.post('/nodes/transform/', transform),
+      list: () => this.get(`/namespaces/${this.namespace}/?type_=transform`)
+    }
+  }
+
+  get dimensions () {
+    return {
+      create: (dimension) => this.post('/nodes/dimension/', dimension),
+      list: () => this.get(`/namespaces/${this.namespace}/?type_=dimension`),
+      link: (nodeName, column, dimension, dimensionColumn) => this.post(`/nodes/${nodeName}/columns/${column}/?dimension=${dimension}&dimension_column=${dimensionColumn}`)
+    }
+  }
+
+  get metrics () {
+    return {
+      create: (metric) => this.post('/nodes/metric/', metric),
+      list: () => this.get(`/namespaces/${this.namespace}/?type_=metric`)
+    }
+  }
+
+  get cubes () {
+    return {
+      get: (cube) => this.post(`/cubes/${cube}/`),
+      create: (cube) => this.post('/nodes/cube/', cube)
     }
   }
 
   get tags () {
     return {
-      get: () => this.get('/tags/')
+      list: () => this.get('/tags/'),
+      get: (tag) => this.get(`/tags/${tag}/nodes/`),
+      create: (tagData) => this.post('/tags/', tagData),
+      update: (tag, tagData) => this.patch(`/tags/${tag}/`, tagData),
+      set: (nodeName, tagName) => this.post(`/nodes/${nodeName}/tag/?tag_name=${tagName}`)
     }
   }
 
   get attributes () {
     return {
-      get: () => this.get('/attributes/')
+      list: () => this.get('/attributes/'),
+      create: (attributeData) => this.post('/attributes/', attributeData)
+    }
+  }
+
+  get materializationConfigs () {
+    return {
+      update: (nodeName, materializationConfig) => this.patch(`/nodes/${nodeName}/materialization/`, materializationConfig)
+    }
+  }
+
+  get columnAttributes () {
+    return {
+      set: (nodeName, columnAttribute) => this.post(`/nodes/${nodeName}/attributes/`, columnAttribute)
+    }
+  }
+
+  get availabilityState () {
+    return {
+      set: (nodeName, availabilityState) => this.post(`/data/${nodeName}/availability/`, availabilityState)
+    }
+  }
+
+  get sql () {
+    return {
+      get: (
+        metrics,
+        dimensions,
+        filters,
+        async_,
+        engineName,
+        engineVersion
+      ) => this.post(`/sql/?metrics=${metrics}&dimensions=${dimensions}&filters=${filters}&async_=${async_}&engine_name=${engineName}&engine_version=${engineVersion}`)
+    }
+  }
+
+  get data () {
+    return {
+      get: (
+        metrics,
+        dimensions,
+        filters,
+        async_,
+        engineName,
+        engineVersion
+      ) => this.post(`/data/?metrics=${metrics}&dimensions=${dimensions}&filters=${filters}&async_=${async_}&engine_name=${engineName}&engine_version=${engineVersion}`)
     }
   }
 }
