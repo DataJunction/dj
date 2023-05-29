96551054afd391edb649c16493a8a1ec5fb2f350
samredai <sredai@netflix.com>
2023-05-29 14:51:09 -0400

    linters

--- alembic/versions/2023_05_29_1821-2ff273e3b5de_history_table.py
+++ alembic/versions/2023_05_29_1821-2ff273e3b5de_history_table.py
@@ -9,30 +9,31 @@ Create Date: 2023-05-29 18:21:26.468042+00:00
 
 import sqlalchemy as sa
 import sqlmodel
+
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
-revision = '2ff273e3b5de'
-down_revision = 'f8c3e437261b'
+revision = "2ff273e3b5de"
+down_revision = "f8c3e437261b"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_table('history',
-    sa.Column('pre', sa.JSON(), nullable=True),
-    sa.Column('post', sa.JSON(), nullable=True),
-    sa.Column('details', sa.JSON(), nullable=True),
-    sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('entity_type', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
-    sa.Column('entity_name', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
-    sa.Column('activity_type', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
-    sa.Column('user', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
-    sa.PrimaryKeyConstraint('id', name=op.f('pk_history'))
+    op.create_table(
+        "history",
+        sa.Column("pre", sa.JSON(), nullable=True),
+        sa.Column("post", sa.JSON(), nullable=True),
+        sa.Column("details", sa.JSON(), nullable=True),
+        sa.Column("created_at", sa.DateTime(timezone=True), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("entity_type", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+        sa.Column("entity_name", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+        sa.Column("activity_type", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+        sa.Column("user", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_history")),
     )
 
 
 def downgrade():
-    op.drop_table('history')
+    op.drop_table("history")
