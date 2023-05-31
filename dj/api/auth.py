d0b9e9ba2e585b4760560751454197824bf989b0
samredai <sredai@netflix.com>
2023-05-31 00:56:23 -0400

    add a username=dj, password=dj user

--- dj/api/auth.py
+++ dj/api/auth.py
@@ -2,9 +2,9 @@
 Auth dependencies
 """
 import logging
-from typing import Annotated
+from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Security
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from dj.models.user import Token, User
@@ -16,12 +16,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 async def get_current_user(
-    token: Annotated[str, Depends(oauth2_scheme)],  # pylint: disable=unused-argument
+    token: str = Security(oauth2_scheme),  # pylint: disable=unused-argument
 ) -> User:  # pylint: disable=unused-argument
     """
     Overrideable placeholder function for getting a user from a token
     """
-    return User(username="unknown")
+    return User(username="admin")
 
 
 class PermissionsChecker:  # pylint: disable=too-few-public-methods
@@ -51,4 +51,9 @@ async def generate_an_auth_token(
     """
     Overrideable placeholder endpoint for generating a bearer token
     """
-    return {"access_token": "unsecured", "token_type": "bearer"}
+    if form_data.username != "dj" or form_data.password != "dj":
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorrect username and/or password",
+        )
+    return {"access_token": "unsecure", "token_type": "bearer"}
