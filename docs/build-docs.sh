283232d8d5b26808f6410354c3ce3cddbb3f7362
Samuel Redai <samuelspersonalemail@gmail.com>
2023-06-22 11:31:40 -0400

    Fix typo in arg, baseUrl -> baseURL (#593)
    * Fix typo in arg, baseUrl -> baseURL
    
    * Include BASE_URL in cli docs

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -2,10 +2,11 @@ if [ $# -eq 0 ]; then
     echo
     echo "Build a specified version of the DJ docs"
     echo
-    echo "Syntax: ./build-docs.sh <VERSION> <SET_AS_LATEST>"
+    echo "Syntax: ./build-docs.sh <VERSION> <SET_AS_LATEST> <BASE_URL>"
     echo
     echo "VERSION: The version of the docs site to build, i.e. 0.1.0"
     echo "SET_AS_LATEST: Additionally build the specified version as the default/latest docs site, i.e. true"
+    echo "BASE_URL: The base URL to set for the build"
     echo
     exit 1
 fi
@@ -15,11 +16,11 @@ SET_AS_LATEST=$2   # false
 BASE_URL=$3        # http://localhost:55555
 
 if [ "$SET_AS_LATEST" = true ] ; then
-    hugo -d public --contentDir content/${VERSION}/ --configDir config --baseUrl $BASE_URL --cleanDestinationDir
+    hugo -d public --contentDir content/${VERSION}/ --configDir config --baseURL $BASE_URL --cleanDestinationDir
 fi
 
 # Build the versions site (sits outside of docs sites for each version)
-hugo -d public/versions --contentDir content/versions/ --configDir config --baseUrl $BASE_URL --environment versions --cleanDestinationDir
+hugo -d public/versions --contentDir content/versions/ --configDir config --baseURL $BASE_URL --environment versions --cleanDestinationDir
 
 # Build the docs site for specified version
-hugo -d public/${VERSION} --contentDir content/${VERSION}/ --configDir config --baseUrl "${BASE_URL}${VERSION}" --cleanDestinationDir
+hugo -d public/${VERSION} --contentDir content/${VERSION}/ --configDir config --baseURL "${BASE_URL}${VERSION}" --cleanDestinationDir
