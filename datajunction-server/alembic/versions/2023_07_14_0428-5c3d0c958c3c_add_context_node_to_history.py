44a081968b3e1e7f68fbd5001e62d18ca00e1201
Yian Shang <yian.shang@gmail.com>
2023-07-13 21:42:10 -0700

    Add alembic migration for context node in history

---
+++ datajunction-server/alembic/versions/2023_07_14_0428-5c3d0c958c3c_add_context_node_to_history.py
@@ -0,0 +1,27 @@
+"""Add context node to history
+
+Revision ID: 5c3d0c958c3c
+Revises: 4e1ff36c27c6
+Create Date: 2023-07-14 04:28:11.334887+00:00
+
+"""
+# pylint: disable=no-member, invalid-name, missing-function-docstring, unused-import, no-name-in-module
+
+import sqlalchemy as sa
+import sqlmodel
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '5c3d0c958c3c'
+down_revision = '4e1ff36c27c6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('history', sa.Column('context_node', sqlmodel.sql.sqltypes.AutoString(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('history', 'context_node')
