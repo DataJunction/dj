c2c0e085d79c9f593f5fda0bbaa043be352d92db
Yian Shang <yian.shang@gmail.com>
2023-07-20 13:20:00 -0700

    Add more context from column's parent when type inference for a column fails

--- client/python/tests/test_client.py
+++ client/python/tests/test_client.py
@@ -463,7 +463,12 @@ class TestDJReader:
         )
         assert (
             result["message"]
-            == "Cannot resolve type of column dimension_that_does_not_exist."
+            == "Cannot resolve type of column dimension_that_does_not_exist in SELECT  "
+            "dimension_that_does_not_exist,\n"
+            "\tavg(foo_DOT_bar_DOT_repair_order_details.price) "
+            "foo_DOT_bar_DOT_avg_repair_price \n"
+            " FROM roads.repair_order_details AS foo_DOT_bar_DOT_repair_order_details \n"
+            " GROUP BY  dimension_that_does_not_exist\n"
         )
 
         # Retrieve SQL for multiple metrics using the client object
