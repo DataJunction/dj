9b87de8853e61d441a4466d3cca00d09f5088c5c
Yian Shang <yian.shang@gmail.com>
2023-07-21 07:42:57 -0700

    Add functionality to get nodes with common dimensions

--- datajunction-server/datajunction_server/api/dimensions.py
+++ datajunction-server/datajunction_server/api/dimensions.py
@@ -2,14 +2,17 @@
 Dimensions related APIs.
 """
 import logging
-from typing import List, Optional
+from typing import Annotated, List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session
 
 from datajunction_server.api.helpers import get_node_by_name
 from datajunction_server.models.node import NodeRevisionOutput, NodeType
-from datajunction_server.sql.dag import get_nodes_with_dimension
+from datajunction_server.sql.dag import (
+    get_nodes_with_common_dimensions,
+    get_nodes_with_dimension,
+)
 from datajunction_server.utils import get_session
 
 _logger = logging.getLogger(__name__)
@@ -28,4 +31,22 @@ def find_nodes_with_dimension(
     """
     dimension_node = get_node_by_name(session, name)
     nodes = get_nodes_with_dimension(session, dimension_node, node_types)
+    return nodes
+
+
+@router.get("/dimensions/common/", response_model=List[NodeRevisionOutput])
+def find_nodes_with_common_dimensions(
+    dimension: Annotated[list[str] | None, Query()] = Query(None),
+    *,
+    node_types: Optional[List[NodeType]] = None,
+    session: Session = Depends(get_session),
+) -> List[NodeRevisionOutput]:
+    """
+    List all nodes that have the specified dimension
+    """
+    nodes = get_nodes_with_common_dimensions(
+        session,
+        [get_node_by_name(session, dim) for dim in dimension],  # type: ignore
+        node_types,
+    )
     return nodes
--- datajunction-server/tests/api/dimensions_test.py
+++ datajunction-server/tests/api/dimensions_test.py
@@ -40,3 +40,39 @@ def test_list_nodes_with_dimension(client_with_examples: TestClient) -> None:
     assert [node["name"] for node in data] == [
         "default.repair_type",
     ]
+
+
+def test_list_nodes_with_common_dimension(client_with_examples: TestClient) -> None:
+    """
+    Test ``GET /dimensions/common/``.
+    """
+    response = client_with_examples.get(
+        "/dimensions/common/?dimension=default.hard_hat",
+    )
+    data = response.json()
+    roads_nodes = [
+        "default.repair_orders",
+        "default.repair_order_details",
+        "default.num_repair_orders",
+        "default.avg_repair_price",
+        "default.total_repair_cost",
+        "default.discounted_orders_rate",
+        "default.total_repair_order_discounts",
+        "default.avg_repair_order_discounts",
+        "default.avg_time_to_dispatch",
+    ]
+    assert [node["name"] for node in data] == roads_nodes
+
+    response = client_with_examples.get(
+        "/dimensions/common/?dimension=default.hard_hat&dimension=default.us_state"
+        "&dimension=default.dispatcher&dimension=default.municipality_dim",
+    )
+    data = response.json()
+    assert [node["name"] for node in data] == roads_nodes
+
+    response = client_with_examples.get(
+        "/dimensions/common/?dimension=default.hard_hat&dimension=default.us_state"
+        "&dimension=default.dispatcher&dimension=default.payment_type",
+    )
+    data = response.json()
+    assert [node["name"] for node in data] == []
