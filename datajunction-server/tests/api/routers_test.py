26eb316dcae1e4b92177197510b20af42cf2f943
Yian Shang <yian.shang@gmail.com>
2023-09-01 07:49:42 -0700

    Updating routers and ast tests to use the smaller test client fixtures

--- datajunction-server/tests/api/routers_test.py
+++ datajunction-server/tests/api/routers_test.py
@@ -5,28 +5,28 @@ from fastapi.testclient import TestClient
 
 
 def test_api_router_trailing_slashes(
-    client_with_examples: TestClient,
+    client_with_basic: TestClient,
 ) -> None:
     """
     Test that the API router used by our endpoints will send both routes without
     trailing slashes and those with trailing slashes to right place.
     """
-    response = client_with_examples.get("/attributes/")
+    response = client_with_basic.get("/attributes/")
     assert response.ok
     assert len(response.json()) > 0
 
-    response = client_with_examples.get("/attributes")
+    response = client_with_basic.get("/attributes")
     assert response.ok
     assert len(response.json()) > 0
 
-    response = client_with_examples.get("/namespaces/")
+    response = client_with_basic.get("/namespaces/")
     assert response.ok
     assert len(response.json()) > 0
 
-    response = client_with_examples.get("/namespaces")
+    response = client_with_basic.get("/namespaces")
     assert response.ok
     assert len(response.json()) > 0
 
-    response = client_with_examples.get("/namespaces/basic?type_=source")
+    response = client_with_basic.get("/namespaces/basic?type_=source")
     assert response.ok
     assert len(response.json()) > 0
