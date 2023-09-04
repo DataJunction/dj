749456510ede25e5f0d63589e6307fe043e944ac
Yian Shang <yian.shang@gmail.com>
2023-09-04 12:27:42 -0700

    Add additional test for TranslatedSQL

--- datajunction-server/tests/transpilation_test.py
+++ datajunction-server/tests/transpilation_test.py
@@ -1,9 +1,19 @@
 """Tests the transpilation plugins."""
 import pytest
+from pytest_mock import MockerFixture
 
 from datajunction_server.errors import DJPluginNotFoundException
 from datajunction_server.models.engine import Dialect
+from datajunction_server.models.metric import TranslatedSQL
 from datajunction_server.transpilation import get_transpilation_plugin
+
+
+class MockSettings:  # pylint: disable=too-few-public-methods
+    """
+    Mock settings object
+    """
+
+    sql_transpilation_library = "sqlglot"
 
 
 def test_get_transpilation_plugin() -> None:
@@ -30,3 +40,18 @@ def test_get_transpilation_plugin() -> None:
         == "1"
     )
     assert known.transpile_sql("1", output_dialect=Dialect.TRINO) == "1"
+
+
+def test_translated_sql(mocker: MockerFixture) -> None:
+    """
+    Verify that the TranslatedSQL object will call the configured transpilation plugin
+    """
+    mock_settings = mocker.MagicMock()
+    mock_settings.return_value = MockSettings()
+    mocker.patch("datajunction_server.models.metric.get_settings", mock_settings)
+    translated_sql = TranslatedSQL(
+        sql="1",
+        columns=[],
+        dialect=Dialect.SPARK,
+    )
+    assert translated_sql.sql == "1"
