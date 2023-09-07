fd5dafa7b40edb8267d5438f87fb2e5306746c81
Yian Shang <yian.shang@gmail.com>
2023-09-07 07:43:30 -0700

    Fix warnings with tests and errors where the dom on different tests was interacting with each other and causing failures. Get to 100% test coverage for AddEditNodePage

--- datajunction-ui/src/setupTests.ts
+++ datajunction-ui/src/setupTests.ts
@@ -4,3 +4,30 @@ import '@testing-library/jest-dom';
 
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import { JSDOM } from 'jsdom';
+import { render as originalRender } from '@testing-library/react';
+
+const setDom = () => {
+  const dom = new JSDOM('<!doctype html><html><body></body></html>', {});
+
+  global.window = dom.window;
+  global.document = dom.window.document;
+  document.createRange = () => {
+    const range = new Range();
+    range.getBoundingClientRect = jest.fn();
+    range.getClientRects = () => {
+      return {
+        item: () => null,
+        length: 0,
+        [Symbol.iterator]: jest.fn(),
+      };
+    };
+
+    return range;
+  };
+};
+
+export const render = ui => {
+  setDom();
+  return originalRender(ui);
+};
