70156c830e9ecc12c63b73ec23b0343d7d755df8
Yian Shang <yian.shang@gmail.com>
2023-09-08 09:11:39 -0700

    Add tests for Login page

---
+++ datajunction-ui/src/app/pages/LoginPage/__tests__/index.test.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { LoginPage } from '../index';
+
+describe('LoginPage', () => {
+  const reloadFn = () => {
+    window.location.reload();
+  };
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: jest.fn() },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: original,
+    });
+  });
+
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('displays error messages when fields are empty and form is submitted', async () => {
+    const { getByText, queryAllByText } = render(<LoginPage />);
+    fireEvent.click(getByText('Login'));
+
+    await waitFor(() => {
+      expect(getByText('DataJunction')).toBeInTheDocument();
+      expect(queryAllByText('Required').length).toEqual(2);
+    });
+  });
+
+  it('calls fetch with correct data on submit', async () => {
+    const username = 'testUser';
+    const password = 'testPassword';
+    reloadFn();
+
+    const { getByText, getByPlaceholderText } = render(<LoginPage />);
+    fireEvent.change(getByPlaceholderText('Username'), {
+      target: { value: username },
+    });
+    fireEvent.change(getByPlaceholderText('Password'), {
+      target: { value: password },
+    });
+    fireEvent.click(getByText('Login'));
+
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalledWith(
+        `${process.env.REACT_APP_DJ_URL}/basic/login/`,
+        expect.objectContaining({
+          method: 'POST',
+          body: expect.any(FormData),
+          credentials: 'include',
+        }),
+      );
+      expect(window.location.reload).toHaveBeenCalled();
+    });
+  });
+});
