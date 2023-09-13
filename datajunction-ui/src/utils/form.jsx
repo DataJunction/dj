b5143df61cc17002b9425b6a698ad752548955cb
Yian Shang <yian.shang@gmail.com>
2023-09-12 20:21:23 -0700

    Add UI for editing column attributes on the node column tab

---
+++ datajunction-ui/src/utils/form.jsx
@@ -0,0 +1,23 @@
+import ValidIcon from '../app/icons/ValidIcon';
+import AlertIcon from '../app/icons/AlertIcon';
+import * as React from 'react';
+
+export const displayMessageAfterSubmit = status => {
+  return status?.success !== undefined ? (
+    <div className="message success">
+      <ValidIcon />
+      {status?.success}
+    </div>
+  ) : status?.failure !== undefined ? (
+    <div className="message alert">
+      <AlertIcon />
+      {status?.failure}
+    </div>
+  ) : (
+    ''
+  );
+};
+
+export const labelize = name => {
+  return name.replace(/_/g, ' ').replace(/\b(\w)/g, char => char.toUpperCase());
+};
