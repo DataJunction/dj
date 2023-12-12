e6c9fee8b8c2e0a7c88a3a1b4b5e2bc307991b75
Yian Shang <yian.shang@gmail.com>
2023-12-11 20:04:28 -0800

    Fix enum str value

--- datajunction-server/datajunction_server/enum.py
+++ datajunction-server/datajunction_server/enum.py
@@ -14,5 +14,8 @@ else:
     class StrEnum(str, enum.Enum):  # pragma: no cover
         """Backwards compatible StrEnum for Python < 3.11"""  # pragma: no cover
 
+        def __str__(self):
+            return str(self.value)
+
     class IntEnum(int, enum.Enum):  # pragma: no cover
         """Backwards compatible IntEnum for Python < 3.11"""  # pragma: no cover
