5b38ac716a4f8bf38472da63178d6231bcb18b89
Yian Shang <yian.shang@gmail.com>
2024-04-28 23:52:58 -0700

    Save built queries to database to speed up requesting SQL and data

--- datajunction-server/datajunction_server/api/sql.py
+++ datajunction-server/datajunction_server/api/sql.py
@@ -15,6 +15,9 @@ from datajunction_server.api.helpers import (
     validate_orderby,
 )
 from datajunction_server.construction.build import get_measures_query
+from datajunction_server.construction.utils import to_namespaced_name
+from datajunction_server.database import Node, QueryRequest
+from datajunction_server.database.queryrequest import QueryBuildType
 from datajunction_server.database.user import User
 from datajunction_server.internal.access.authentication.http import SecureAPIRouter
 from datajunction_server.internal.access.authorization import validate_access
@@ -22,6 +25,8 @@ from datajunction_server.internal.engines import get_engine
 from datajunction_server.models import access
 from datajunction_server.models.metric import TranslatedSQL
 from datajunction_server.models.user import UserOutput
+from datajunction_server.sql.parsing import ast
+from datajunction_server.sql.parsing.backends.antlr4 import parse
 from datajunction_server.utils import get_current_user, get_session, get_settings
 
 _logger = logging.getLogger(__name__)
@@ -35,6 +40,13 @@ async def get_measures_sql_for_cube(
     dimensions: List[str] = Query([]),
     filters: List[str] = Query([]),
     *,
+    include_all_columns: bool = Query(
+        False,
+        description=(
+            "Whether to include all columns or only those necessary "
+            "for the metrics and dimensions in the cube"
+        ),
+    ),
     session: AsyncSession = Depends(get_session),
     engine_name: Optional[str] = None,
     engine_version: Optional[str] = None,
@@ -48,7 +60,26 @@ async def get_measures_sql_for_cube(
     This SQL can be used to produce an intermediate table with all the measures
     and dimensions needed for an analytics database (e.g., Druid).
     """
-    return await get_measures_query(
+    if query_request := await QueryRequest.get_query_request(
+        session,
+        nodes=metrics,
+        dimensions=dimensions,
+        filters=filters,
+        orderby=[],
+        limit=None,
+        engine_name=engine_name,
+        engine_version=engine_version,
+        query_type=QueryBuildType.MEASURES,
+        other_args={"include_all_columns": include_all_columns},
+    ):
+        engine = await get_engine(session, engine_name, engine_version)
+        return TranslatedSQL(
+            sql=query_request.query,
+            columns=query_request.columns,
+            dialect=engine.dialect if engine else None,
+        )
+
+    measures_query = await get_measures_query(
         session=session,
         metrics=metrics,
         dimensions=dimensions,
@@ -57,7 +88,23 @@ async def get_measures_sql_for_cube(
         engine_version=engine_version,
         current_user=current_user,
         validate_access=validate_access,
+        include_all_columns=include_all_columns,
     )
+
+    await QueryRequest.save_query_request(
+        session=session,
+        nodes=metrics,
+        dimensions=dimensions,
+        filters=filters,
+        orderby=[],
+        limit=None,
+        engine_name=engine_name,
+        engine_version=engine_version,
+        query_type=QueryBuildType.MEASURES,
+        query=measures_query.sql,
+        columns=[col.dict() for col in measures_query.columns],
+    )
+    return measures_query
 
 
 @router.get(
@@ -95,6 +142,24 @@ async def get_sql(
         else None
     )
     validate_orderby(orderby, [node_name], dimensions)
+    access_control.validate_access()
+    if query_request := await QueryRequest.get_query_request(
+        session,
+        nodes=[node_name],
+        dimensions=dimensions,
+        filters=filters,
+        orderby=orderby,
+        limit=limit,
+        engine_name=engine.name if engine else None,
+        engine_version=engine.version if engine else None,
+        query_type=QueryBuildType.NODE,
+    ):
+        return TranslatedSQL(
+            sql=query_request.query,
+            columns=query_request.columns,
+            dialect=engine.dialect if engine else None,
+        )
+
     query_ast = await get_query(
         session=session,
         node_name=node_name,
@@ -109,8 +174,22 @@ async def get_sql(
         assemble_column_metadata(col)  # type: ignore
         for col in query_ast.select.projection
     ]
+    query = str(query_ast)
+    await QueryRequest.save_query_request(
+        session=session,
+        nodes=[node_name],
+        dimensions=dimensions,
+        filters=filters,
+        orderby=orderby,
+        limit=limit,
+        engine_name=engine.name,
+        engine_version=engine.version,
+        query_type=QueryBuildType.NODE,
+        query=query,
+        columns=[col.dict() for col in columns],
+    )
     return TranslatedSQL(
-        sql=str(query_ast),
+        sql=query,
         columns=columns,
         dialect=engine.dialect if engine else None,
     )
@@ -142,6 +221,24 @@ async def get_sql_for_metrics(
         base_verb=access.ResourceRequestVerb.READ,
     )
 
+    if query_request := await QueryRequest.get_query_request(
+        session,
+        nodes=metrics,
+        dimensions=dimensions,
+        filters=filters,
+        orderby=orderby,
+        limit=limit,
+        engine_name=engine_name,
+        engine_version=engine_version,
+        query_type=QueryBuildType.METRICS,
+    ):
+        engine = await get_engine(session, engine_name, engine_version)
+        return TranslatedSQL(
+            sql=query_request.query,
+            columns=query_request.columns,
+            dialect=engine.dialect if engine else None,
+        )
+
     translated_sql, _, _ = await build_sql_for_multiple_metrics(
         session,
         metrics,
@@ -152,5 +249,19 @@ async def get_sql_for_metrics(
         engine_name,
         engine_version,
         access_control,
+    )
+
+    await QueryRequest.save_query_request(
+        session=session,
+        nodes=metrics,
+        dimensions=dimensions,
+        filters=filters,
+        orderby=orderby,
+        limit=limit,
+        engine_name=engine_name,
+        engine_version=engine_version,
+        query_type=QueryBuildType.METRICS,
+        query=translated_sql.sql,
+        columns=[col.dict() for col in translated_sql.columns],
     )
     return translated_sql
---
+++ datajunction-server/datajunction_server/database/queryrequest.py
@@ -0,0 +1,236 @@
+"""Partition database schema."""
+from typing import Any, Dict, List, Optional
+
+from sqlalchemy import JSON, BigInteger, Enum, Index, and_, select
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
+from datajunction_server.construction.utils import to_namespaced_name
+from datajunction_server.database import Node
+from datajunction_server.database.base import Base
+from datajunction_server.enum import StrEnum
+from datajunction_server.sql.parsing import ast
+from datajunction_server.sql.parsing.backends.antlr4 import parse
+
+
+class QueryBuildType(StrEnum):
+    """
+    Query building type.
+
+    There are three types of query building patterns that DJ supports:
+    * Metrics SQL
+    * Measures SQL
+    * Node SQL
+    """
+
+    METRICS = "metrics"
+    MEASURES = "measures"
+    NODE = "node"
+
+
+class QueryRequest(Base):  # type: ignore  # pylint: disable=too-few-public-methods
+    """
+    A query request represents a request for DJ to build a query.
+
+    There are three types of query building patterns that DJ supports:
+    * Metrics SQL: query by metrics, dimensions, filters, ...
+    * Measures SQL: query by metrics, dimensions, filters, ...
+    * Node SQL: query by node name, dimensions, filters, ...
+    """
+
+    __tablename__ = "queryrequest"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger(),
+        primary_key=True,
+    )
+
+    query_type: Mapped[str] = mapped_column(Enum(QueryBuildType))
+
+    nodes: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
+    dimensions: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
+    filters: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
+    limit: Mapped[Optional[int]]
+    orderby: Mapped[List[str]] = mapped_column(JSONB, nullable=True)
+    engine_name: Mapped[Optional[str]]
+    engine_version: Mapped[Optional[str]]
+    other_args: Mapped[JSON] = mapped_column(JSONB, nullable=True)
+
+    # Values needed to rebuild a TranslatedSQL object
+    query: Mapped[str]
+    columns: Mapped[JSON] = mapped_column(JSONB, nullable=True)
+
+    @classmethod
+    async def get_query_request(
+        cls,
+        session: AsyncSession,
+        query_type: QueryBuildType,
+        nodes: List[str],
+        dimensions: List[str],
+        filters: List[str],
+        engine_name: Optional[str],
+        engine_version: Optional[str],
+        limit: Optional[int],
+        orderby: List[str],
+        other_args: Optional[Dict[str, Any]] = None,
+    ) -> Optional["QueryRequest"]:
+        """
+        Retrieves saved query for a node SQL request
+        """
+        versioned_request = await cls.to_versioned_query_request(
+            session,
+            nodes,
+            dimensions,
+            filters,
+            orderby,
+        )
+        statement = select(cls).where(
+            and_(
+                cls.query_type == query_type,
+                cls.nodes == versioned_request["nodes"],
+                cls.dimensions == versioned_request["dimensions"],
+                cls.filters == versioned_request["filters"],
+                cls.engine_name == engine_name,
+                cls.engine_version == engine_version,
+                cls.limit == limit,
+                cls.orderby == orderby,
+                cls.other_args == other_args,
+            ),
+        )
+        query_request = (await session.execute(statement)).scalar_one_or_none()
+        if query_request:
+            return query_request
+        return None
+
+    @classmethod
+    async def save_query_request(
+        cls,
+        session: AsyncSession,
+        query_type: QueryBuildType,
+        nodes: List[str],
+        dimensions: List[str],
+        filters: List[str],
+        engine_name: Optional[str],
+        engine_version: Optional[str],
+        limit: Optional[int],
+        orderby: List[str],
+        query: str,
+        columns: List[Dict[str, Any]],
+        other_args: Optional[Dict[str, Any]] = None,
+    ):
+        """
+        Retrieves saved query for a node SQL request
+        """
+        query_request = await cls.get_query_request(
+            session,
+            query_type=query_type,
+            nodes=nodes,
+            dimensions=dimensions,
+            filters=filters,
+            engine_name=engine_name,
+            engine_version=engine_version,
+            limit=limit,
+            orderby=orderby,
+        )
+        if query_request:
+            query_request.query = query
+            query_request.columns = columns
+            session.add(query_request)
+            await session.commit()
+        else:
+            versioned_request = await cls.to_versioned_query_request(
+                session,
+                nodes,
+                dimensions,
+                filters,
+                orderby,
+            )
+            query_request = QueryRequest(
+                query_type=query_type,
+                nodes=versioned_request["nodes"],
+                dimensions=versioned_request["dimensions"],
+                filters=versioned_request["filters"],
+                engine_name=engine_name,
+                engine_version=engine_version,
+                limit=limit,
+                orderby=orderby,
+                query=query,
+                columns=columns,
+                other_args=other_args,
+            )
+            session.add(query_request)
+            await session.commit()
+
+    @classmethod
+    async def to_versioned_query_request(
+        cls,
+        session: AsyncSession,
+        nodes: List[str],
+        dimensions: List[str],
+        filters: List[str],
+        orderby: List[str],
+    ) -> Dict[str, List[str]]:
+        """
+        Prepare for searching in saved query requests by appending version numbers to all nodes
+        being worked with, from the nodes we're retrieving the queries of to the
+        """
+        nodes = [await Node.get_by_name(session, node, options=[]) for node in nodes]
+        dimension_nodes = [
+            await Node.get_by_name(session, ".".join(dim.split(".")[:-1]), options=[])
+            for dim in dimensions
+        ]
+        filter_asts = {
+            filter_: parse(f"SELECT 1 WHERE {filter_}") for filter_ in filters
+        }
+        for filter_ in filter_asts:
+            for col in filter_asts[filter_].select.where.find_all(ast.Column):
+                dimension_node = await Node.get_by_name(
+                    session,
+                    col.alias_or_name.namespace.identifier(),
+                    options=[],
+                )
+                col.alias_or_name.namespace = to_namespaced_name(
+                    f"{dimension_node.name}@{dimension_node.current_version}",
+                )
+
+        for idx, order in enumerate(orderby):
+            order_rule = order.split(" ")
+            metric = await Node.get_by_name(session, order_rule[0], options=[])
+            if metric:
+                order_rule[0] = f"{metric.name}@{metric.current_version}"
+                orderby[idx] = " ".join(order_rule)
+            else:
+                node = await Node.get_by_name(
+                    session,
+                    ".".join(order_rule[0].split(".")[:-1]),
+                    options=[],
+                )
+                order_rule[0].replace(node.name, f"{node.name}@{node.current_version}")
+                orderby[idx] = " ".join(order_rule)
+
+        return {
+            "nodes": [f"{node.name}@{node.current_version}" for node in nodes],
+            "dimensions": [
+                f"{dim}@{node.current_version}"
+                for node, dim in zip(dimension_nodes, dimensions)
+            ],
+            "filters": [
+                str(filter_ast.select.where) for filter_ast in filter_asts.values()
+            ],
+            "orderby": orderby,
+        }
+
+
+Index(
+    "query_request_unique",
+    QueryRequest.query_type,
+    QueryRequest.nodes,
+    QueryRequest.dimensions,
+    QueryRequest.filters,
+    QueryRequest.engine_name,
+    QueryRequest.engine_version,
+    QueryRequest.limit,
+    QueryRequest.orderby,
+    unique=True,
+)
