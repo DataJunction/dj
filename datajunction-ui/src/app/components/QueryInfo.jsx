9d777dc236eeb290b51fc70b0b538fa81d170fff
Yian Shang <yian.shang@gmail.com>
2024-05-07 21:52:09 -0700

    Add node query generation with filters

--- datajunction-ui/src/app/components/QueryInfo.jsx
+++ datajunction-ui/src/app/components/QueryInfo.jsx
@@ -9,8 +9,9 @@ export default function QueryInfo({
   scheduled,
   started,
   numRows,
+  isList = false,
 }) {
-  return (
+  return isList === false ? (
     <div className="table-responsive">
       <table className="card-inner-table table">
         <thead className="fs-7 fw-bold text-gray-400 border-bottom-0">
@@ -72,6 +73,66 @@ export default function QueryInfo({
           </tr>
         </tbody>
       </table>
+    </div>
+  ) : (
+    <div className="rightbottom">
+      <ul style={{ padding: '20px' }}>
+        <li className={'query-info'}>
+          <label>Query ID:</label>{' '}
+          <span className="rounded-pill badge bg-secondary-soft">{id}</span>
+        </li>
+        <li className={'query-info'}>
+          <label>Engine:</label>{' '}
+          <span className="rounded-pill badge bg-secondary-soft">
+            {engine_name}
+            {' - '}
+            {engine_version}
+          </span>
+        </li>
+        <li className={'query-info'}>
+          <label>State:</label> {state}
+        </li>
+        <li className={'query-info'}>
+          <label>Scheduled:</label> {scheduled}
+        </li>
+        <li className={'query-info'}>
+          <label>Started:</label> {started}
+        </li>
+        <li className={'query-info'}>
+          <label>Errors:</label>{' '}
+          {errors?.length ? (
+            errors.map((e, idx) => (
+              <p key={`error-${idx}`}>
+                <span className="rounded-pill badge bg-secondary-error">
+                  {e}
+                </span>
+              </p>
+            ))
+          ) : (
+            <></>
+          )}
+        </li>
+        <li className={'query-info'}>
+          <label>Links:</label>{' '}
+          {links?.length ? (
+            links.map((link, idx) => (
+              <p key={idx}>
+                <a href={link} target="_blank" rel="noreferrer">
+                  {link}
+                </a>
+              </p>
+            ))
+          ) : (
+            <></>
+          )}
+        </li>
+        <li className={'query-info'}>
+          <label>Output Table:</label> {output_table}
+        </li>
+        <li className={'query-info'}>
+          <label>Rows:</label> {numRows}
+        </li>
+      </ul>
     </div>
   );
 }
