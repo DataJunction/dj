1552e47b60e8d5a2a80f76b4c6935e4ba6585d42
samredai <sredai@netflix.com>
2024-07-01 11:19:56 -0400

    change fixtures to module scope

--- datajunction-server/tests/api/collections_test.py
+++ datajunction-server/tests/api/collections_test.py
@@ -12,14 +12,14 @@ class TestCollections:
     """
 
     @pytest.mark.asyncio
-    async def test_creating_a_collection(
+    async def test_collections_creating(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test creating a new collection
         """
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -34,7 +34,7 @@ class TestCollections:
         }
 
         # Ensure the collection shows up in collections list
-        response = await client_with_account_revenue.get(
+        response = await module__client_with_account_revenue.get(
             "/collections/",
         )
         assert response.status_code == 200
@@ -48,7 +48,7 @@ class TestCollections:
         ]
 
         # Ensure that creating the same collection again returns a 409
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -60,15 +60,15 @@ class TestCollections:
         assert "Collection already exists" in str(data)
 
     @pytest.mark.asyncio
-    async def test_adding_to_collection(
+    async def test_collections_adding(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test adding a node to a collection
         """
         # Create a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -78,22 +78,22 @@ class TestCollections:
         assert response.status_code == 201
 
         # Add a node to a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/Accounting/nodes/",
             json=["default.payment_type", "default.revenue"],
         )
         assert response.status_code == 204
 
     @pytest.mark.asyncio
-    async def test_nodes_not_found_when_adding_to_collection(
+    async def test_collections__nodes_not_found_when_adding(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test adding a node to a collection when the nodes can't be found
         """
         # Create a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -103,22 +103,22 @@ class TestCollections:
         assert response.status_code == 201
 
         # Add a node to a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/Accounting/nodes/",
             json=["foo.bar", "baz.qux"],
         )
         assert response.status_code == 404
 
     @pytest.mark.asyncio
-    async def test_removing_from_collection(
+    async def test_collections_removing_nodes(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test removing a node from a collection
         """
         # Create a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -129,36 +129,36 @@ class TestCollections:
         assert response.status_code == 201
 
         # Add a node to a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/Accounting/nodes/",
             json=["default.payment_type"],
         )
         assert response.status_code == 204
 
         # Remove the node from the collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/Accounting/remove/",
             json=["default.payment_type", "default.revenue"],
         )
         assert response.status_code == 204
 
         # Confirm node no longer found in collection
-        response = await client_with_account_revenue.get(
+        response = await module__client_with_account_revenue.get(
             "/collections/Accounting",
         )
         data = response.json()
         assert data["nodes"] == []
 
     @pytest.mark.asyncio
-    async def test_no_errors_when_removing_from_collection(
+    async def test_collections_no_errors_when_removing(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test removing a node from a collection when the node can't be found
         """
         # Create a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -168,22 +168,22 @@ class TestCollections:
         assert response.status_code == 201
 
         # Remove the node from the collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/Accounting/remove/",
             json=["foo.bar", "baz.qux"],
         )
         assert response.status_code == 204
 
     @pytest.mark.asyncio
-    async def test_deleting_collection(
+    async def test_collections_deleting(
         self,
-        client_with_account_revenue: AsyncClient,
+        module__client_with_account_revenue: AsyncClient,
     ) -> None:
         """
         Test removing a collection
         """
         # Create a collection
-        response = await client_with_account_revenue.post(
+        response = await module__client_with_account_revenue.post(
             "/collections/",
             json={
                 "name": "Accounting",
@@ -193,13 +193,13 @@ class TestCollections:
         assert response.status_code == 201
 
         # Remove the collection
-        response = await client_with_account_revenue.delete(
+        response = await module__client_with_account_revenue.delete(
             "/collections/Accounting",
         )
         assert response.status_code == 204
 
         # Ensure the collection does not show up in collections list
-        response = await client_with_account_revenue.get(
+        response = await module__client_with_account_revenue.get(
             "/collections/",
         )
         assert response.status_code == 200
