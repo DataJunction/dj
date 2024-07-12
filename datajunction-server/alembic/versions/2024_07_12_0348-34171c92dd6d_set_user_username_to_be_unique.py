ce3a25c4f17feb7147557e0c33a3ac3659332a56
samredai <sredai@netflix.com>
2024-07-12 00:40:30 -0400

    lint

--- datajunction-server/alembic/versions/2024_07_12_0348-34171c92dd6d_set_user_username_to_be_unique.py
+++ datajunction-server/alembic/versions/2024_07_12_0348-34171c92dd6d_set_user_username_to_be_unique.py
@@ -8,21 +8,21 @@ Create Date: 2024-07-12 03:48:53.609685+00:00
 # pylint: disable=no-member, invalid-name, missing-function-docstring, unused-import, no-name-in-module
 
 import sqlalchemy as sa
+
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
-revision = '34171c92dd6d'
-down_revision = '640a814db2d8'
+revision = "34171c92dd6d"
+down_revision = "640a814db2d8"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table('users', schema=None) as batch_op:
-        batch_op.create_unique_constraint(None, ['username'])
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.create_unique_constraint(None, ["username"])
 
 
 def downgrade():
-    with op.batch_alter_table('users', schema=None) as batch_op:
-        batch_op.drop_constraint(None, type_='unique')
+    with op.batch_alter_table("users", schema=None) as batch_op:
+        batch_op.drop_constraint(None, type_="unique")
