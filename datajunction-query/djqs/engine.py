a2c46b75b41ecfbc589fc1dd5c98703d16ffbff7
sadath-12 <sadathsadu2002@gmail.com>
2024-07-26 21:58:45 +0530

    adjust headers
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- datajunction-query/djqs/engine.py
+++ datajunction-query/djqs/engine.py
@@ -28,10 +28,6 @@ from djqs.typing import ColumnType, Description, SQLADialect, Stream, TypeEnum
 
 _logger = logging.getLogger(__name__)
 
-QUERY_USER = None
-QUERY_PASSWORD = None
-HTTP_SCHEME = None
-
 
 def get_columns_from_description(
     description: Description,
@@ -83,15 +79,12 @@ def create_trino_engine(engine: Engine, headers: Optional[Dict[str, str]]) -> "E
     path = parsed_uri.path.lstrip("/")
     catalog, schema = path.split("/", 1) if "/" in path else (path, None)
 
-    global QUERY_USER, QUERY_PASSWORD, HTTP_SCHEME
+    query_user = headers.get("QUERY_USER") or engine.extra_params["user"]
+    query_password = headers.get("QUERY_PASSWORD") or engine.extra_params["password"]
+    http_scheme = headers.get("TRINO_HTTP_SCHEME") or engine.extra_params["http_scheme"]
 
-    # Override user and password from headers if available
-    if headers and "QUERY_USER" in headers and "QUERY_PASSWORD" in headers:
-        QUERY_USER = headers.get("QUERY_USER")
-        QUERY_PASSWORD = headers.get("QUERY_PASSWORD")
-
-    if HTTP_SCHEME == "https":
-        engine_str = f"{scheme}://{QUERY_USER}:{QUERY_PASSWORD}@{host}:{port}/{catalog}/{schema}?protocol={HTTP_SCHEME}"
+    if http_scheme == "https" and query_user and query_password:
+        engine_str = f"{scheme}://{query_user}:{query_password}@{host}:{port}/{catalog}/{schema}?protocol={http_scheme}"
         return create_engine(engine_str)
     else:
         engine_str = engine.uri
@@ -120,11 +113,6 @@ def run_query(
 
     parsed_uri = urlparse(engine.uri)
     scheme = parsed_uri.scheme
-
-    global QUERY_USER, QUERY_PASSWORD, HTTP_SCHEME
-    QUERY_USER = engine.extra_params["user"]
-    QUERY_PASSWORD = engine.extra_params["password"]
-    HTTP_SCHEME = "https"
 
     if engine.type == EngineType.DUCKDB:
         conn = (
