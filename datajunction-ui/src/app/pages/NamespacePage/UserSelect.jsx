3c7a9c1dbb3e295eece3980a3248b95c58ce6e1e
Yian Shang <yian.shang@gmail.com>
2024-07-28 09:26:08 -0700

    Add filter options to the existing Explore dashboard

---
+++ datajunction-ui/src/app/pages/NamespacePage/UserSelect.jsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from 'react';
+import DJClientContext from '../../providers/djclient';
+import Control from './FieldControl';
+
+import Select from 'react-select';
+
+export default function UserSelect({ onChange }) {
+  const djClient = useContext(DJClientContext).DataJunctionAPI;
+  const [retrieved, setRetrieved] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const users = await djClient.users();
+      console.log('users', users);
+      setUsers(users);
+      setRetrieved(true);
+    };
+    fetchData().catch(console.error);
+  }, [djClient]);
+
+  return (
+    <span className="menu-link" style={{ marginLeft: '30px', width: '400px' }}>
+      {retrieved ? (
+        <Select
+          name="edited_by"
+          isClearable
+          label="Edited By"
+          components={{ Control }}
+          onChange={e => onChange(e)}
+          defaultValue={{
+            value: 'yshang@netflix.com',
+            label: 'yshang@netflix.com',
+          }}
+          options={users?.map(user => {
+            return { value: user.username, label: `${user.username}` };
+          })}
+        />
+      ) : (
+        ''
+      )}
+    </span>
+  );
+}
