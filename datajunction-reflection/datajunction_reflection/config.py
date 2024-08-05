6d2da5638ca0b5df254edf19fc91c23b82205e9f
sadath-12 <sadathsadu2002@gmail.com>
2024-08-05 11:08:35 +0530

    feat: pass env variables for reflection service
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- datajunction-reflection/datajunction_reflection/config.py
+++ datajunction-reflection/datajunction_reflection/config.py
@@ -1,22 +1,19 @@
-"""Reflection service settings."""
 from functools import lru_cache
-
 from pydantic import BaseSettings
-
 
 class Settings(BaseSettings):
     """
     Default settings for the reflection service.
     """
 
-    core_service: str = "http://dj:8000"
-    query_service: str = "http://djqs:8001"
-    celery_broker: str = "redis://djrs-redis:6379/1"
-    celery_results_backend: str = "redis://djrs-redis:6379/2"
+    core_service: str = "http://dj:8000"  # Default value
+    query_service: str = "http://djqs:8001"  # Default value
+    celery_broker: str = "redis://djrs-redis:6379/1"  # Default value
+    celery_results_backend: str = "redis://djrs-redis:6379/2"  # Default value
+    polling_interval: int = 3600  # Default value
 
-    # Set the number of seconds to wait in between polling
-    polling_interval: int = 3600
-
+    class Config:
+        env_prefix = "REFLECTION_"  # Prefix for environment variables
 
 @lru_cache
 def get_settings() -> Settings:
