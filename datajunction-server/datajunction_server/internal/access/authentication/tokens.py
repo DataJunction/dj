d9c85ef384befac6efa4d4754382b387135fb4f9
Yian Shang <yian.shang@gmail.com>
2024-08-26 08:55:39 +0100

    Cleanup

--- datajunction-server/datajunction_server/internal/access/authentication/tokens.py
+++ datajunction-server/datajunction_server/internal/access/authentication/tokens.py
@@ -77,8 +77,5 @@ def decode_token(token: str) -> dict:
         settings.secret,
         algorithms=["HS256"],
         issuer=settings.url,
-        options={
-            "leeway": timedelta(days=3),
-        },
     )
     return decoded_jwt
