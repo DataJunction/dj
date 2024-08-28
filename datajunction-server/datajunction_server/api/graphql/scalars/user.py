38f266299f8d3247ff3aea5de558537ad16c423e
Yian Shang <yian.shang@gmail.com>
2024-08-29 00:48:08 +0100

    Add back for GraphQL user query

--- datajunction-server/datajunction_server/api/graphql/scalars/user.py
+++ datajunction-server/datajunction_server/api/graphql/scalars/user.py
@@ -2,6 +2,7 @@
 User related scalars
 """
 from enum import Enum
+from typing import Optional
 
 import strawberry
 
@@ -25,9 +26,9 @@ class User:  # pylint: disable=too-few-public-methods
     A DataJunction User
     """
 
-    # id: BigInt
+    id: BigInt
     username: str
-    # email: Optional[str]
-    # name: Optional[str]
-    # oauth_provider: OAuthProvider
-    # is_admin: bool
+    email: Optional[str]
+    name: Optional[str]
+    oauth_provider: OAuthProvider
+    is_admin: bool
