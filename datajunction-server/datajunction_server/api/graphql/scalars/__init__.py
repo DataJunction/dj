2cd613607674ada6e48c038b5e205e15ff6d6a91
Yian Shang <yian.shang@gmail.com>
2024-10-10 16:36:03 -0700

    Add separate GQL query for paginated findNode

--- datajunction-server/datajunction_server/api/graphql/scalars/__init__.py
+++ datajunction-server/datajunction_server/api/graphql/scalars/__init__.py
@@ -1,7 +1,7 @@
 """
 GraphQL scalars
 """
-from typing import Union
+from typing import Optional, Union
 
 import strawberry
 
@@ -11,3 +11,16 @@ BigInt = strawberry.scalar(
     parse_value=str,
     description="BigInt field",
 )
+
+
+@strawberry.type
+class PageMeta:  # pylint: disable=too-few-public-methods
+    """
+    Pagination-related metadata
+    """
+
+    prev_cursor: Optional[str] = strawberry.field(description="The previous cursor")
+    next_cursor: Optional[str] = strawberry.field(
+        description="The next cursor to continue with",
+    )
+    count: int = strawberry.field(description="The number of items in this page")
