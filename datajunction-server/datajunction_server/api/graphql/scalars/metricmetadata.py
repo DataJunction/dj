57aa514c84b663e25dbe31b2c8aeb9e537ce1e3d
Yian Shang <yian.shang@gmail.com>
2024-12-05 18:04:40 -0800

    Fix linters

--- datajunction-server/datajunction_server/api/graphql/scalars/metricmetadata.py
+++ datajunction-server/datajunction_server/api/graphql/scalars/metricmetadata.py
@@ -26,19 +26,7 @@ class Unit:  # pylint: disable=too-few-public-methods
 @strawberry.type
 class AggregationRule:
     """
-    The aggregation rule for the measure. If the Aggregability type is LIMITED, the `level` should
-    be specified to highlight the level at which the measure needs to be aggregated in order to
-    support the specified aggregation function.
-
-    For example, consider a metric like COUNT(DISTINCT user_id). It can be decomposed into a
-    single measure with LIMITED aggregability, i.e., it is only aggregatable if the measure is
-    calculated at the `user_id` level:
-    - name: num_users
-      expression: DISTINCT user_id
-      aggregation: COUNT
-      rule:
-        type: LIMITED
-        level: ["user_id"]
+    The aggregation rule for the measure.
     """
 
     type: Aggregability = Aggregability.NONE  # type: ignore
@@ -62,6 +50,7 @@ class ExtractedMeasures:  # pylint: disable=too-few-public-methods
     """
     extracted measures from metric
     """
+
     measures: list[Measure]
     derived_sql: str
 
