b0c2930dbf358d079f59fc769da6cd58eb6594f3
Yian Shang <yian.shang@gmail.com>
2024-12-18 11:58:54 -0800

    Support drag-and-drop of metrics when editing a cube

--- datajunction-ui/src/app/pages/CubeBuilderPage/MetricsSelect.jsx
+++ datajunction-ui/src/app/pages/CubeBuilderPage/MetricsSelect.jsx
@@ -2,9 +2,42 @@
  * A select component for picking metrics.
  */
 import { useField, useFormikContext } from 'formik';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
+import { SortableContainer, SortableElement, arrayMove, SortableHandle } from 'react-sortable-hoc';
 import React, { useContext, useEffect, useState } from 'react';
 import DJClientContext from '../../providers/djclient';
+
+// function arrayMove(array, from, to) {
+//   const slicedArray = array.slice();
+//   slicedArray.splice(
+//     to < 0 ? array.length + to : to,
+//     0,
+//     slicedArray.splice(from, 1)[0]
+//   );
+//   return slicedArray;
+// }
+
+
+const SortableMultiValue = SortableElement(
+  (props) => {
+    // this prevents the menu from being opened/closed when the user clicks
+    // on a value to begin dragging it. ideally, detecting a click (instead of
+    // a drag) would still focus the control and toggle the menu, but that
+    // requires some magic with refs that are out of scope for this example
+    const onMouseDown = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+    const innerProps = { ...props.innerProps, onMouseDown };
+    return <components.MultiValue {...props} innerProps={innerProps} />;
+  }
+);
+
+const SortableMultiValueLabel = SortableHandle(
+  (props) => <components.MultiValueLabel {...props} />
+);
+
+const SortableSelect = SortableContainer(Select);
 
 export const MetricsSelect = ({ cube }) => {
   const djClient = useContext(DJClientContext).DataJunctionAPI;
@@ -16,9 +49,16 @@ export const MetricsSelect = ({ cube }) => {
 
   // All metrics options
   const [metrics, setMetrics] = useState([]);
+  const [selected, setSelected] = useState([]);
 
   // The existing cube's metrics, if editing a cube
   const [defaultMetrics, setDefaultMetrics] = useState([]);
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    const newSelected = arrayMove(selected, oldIndex, newIndex);
+    setSelected(newSelected);
+    setValue(newSelected.map((item) => item)); // Update formik state
+  };
 
   // Get metrics
   useEffect(() => {
@@ -56,18 +96,30 @@ export const MetricsSelect = ({ cube }) => {
       (cube !== undefined && defaultMetrics.length > 0 && metrics.length > 0)
     ) {
       return (
-        <Select
+        <SortableSelect
+          useDragHandle
+          // react-sortable-hoc props:
+          axis="xy"
+          onSortEnd={onSortEnd}
+          distance={10}
           defaultValue={defaultMetrics}
+          getHelperDimensions={({ node }) => node.getBoundingClientRect()}
+          // react-select props:
           options={metrics}
           name="metrics"
           placeholder={`${metrics.length} Available Metrics`}
           onBlur={field.onBlur}
-          onChange={selected => {
-            setValue(getValue(selected));
+          onChange={(selectedOptions) => {
+            setSelected(selectedOptions || []); // Update local state
+            setValue(getValue(selectedOptions));
           }}
           noOptionsMessage={() => 'No metrics found.'}
           isMulti
           isClearable
+          components={{
+            MultiValue: SortableMultiValue,
+            MultiValueLabel: SortableMultiValueLabel,
+          }}
           closeMenuOnSelect={false}
         />
       );
