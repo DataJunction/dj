7ccad24b81079586f56e6e2dae0379705476c148
samredai <sredai@netflix.com>
2025-02-12 15:45:07 -0500

    add filter to notification test

--- datajunction-server/tests/api/notifications_test.py
+++ datajunction-server/tests/api/notifications_test.py
@@ -70,7 +70,9 @@ async def test_notification_preferences(
         },
     )
     assert response.status_code == 201
-    response = await module__client.get("/notifications/")
+    response = await module__client.get(
+        "/notifications/", params={"entity_name": "some_node_name2"}
+    )
     assert response.status_code == 200
     assert len(response.json()) > 0
     assert response.json()[0]["entity_name"] == "some_node_name2"
