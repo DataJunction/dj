5647d9a1a52ea6c73e39b6813e0a09d91ff35143
Yian Shang <yian.shang@gmail.com>
2025-02-23 09:54:12 -0800

    Fix

--- datajunction-server/datajunction_server/models/cube_materialization.py
+++ datajunction-server/datajunction_server/models/cube_materialization.py
@@ -398,7 +398,7 @@ class DruidCubeMaterializationInput(BaseModel):
     Materialization info as passed to the query service.
     """
 
-    id: int
+    id: int | None = None
     name: str
 
     # Frozen cube info at the time of materialization
