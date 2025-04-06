036f7c5950b2aacf4c7dbf44b6c6dae312c006ee
Yian Shang <yian.shang@gmail.com>
2025-04-06 01:33:18 -0700

    Switch the python client to use ruff

--- datajunction-clients/python/datajunction/models.py
+++ datajunction-clients/python/datajunction/models.py
@@ -1,4 +1,5 @@
 """Models used by the DJ client."""
+
 import enum
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
--- datajunction-clients/python/tests/test_models.py
+++ datajunction-clients/python/tests/test_models.py
@@ -1,4 +1,5 @@
 """Tests for models."""
+
 from datajunction.models import QueryState
 
 
