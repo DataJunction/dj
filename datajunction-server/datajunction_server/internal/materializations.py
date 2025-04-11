87ce2311e37af90a8cccb3767b6f07ad8e4cdb2d
Yian Shang <yian.shang@gmail.com>
2025-04-11 02:06:54 -0700

    Fix and add new tests

--- datajunction-server/datajunction_server/internal/materializations.py
+++ datajunction-server/datajunction_server/internal/materializations.py
@@ -173,12 +173,18 @@ async def build_cube_materialization_config(
                     if col.semantic_type == SemanticType.DIMENSION
                 ],
                 measures=metrics_expressions,
-                spark=upsert_input.config.spark,
+                spark=upsert_input.config.spark.__root__
+                if hasattr(upsert_input, "config") and upsert_input.config.spark
+                else {},
                 upstream_tables=measures_query.upstream_tables,
                 columns=measures_query.columns,
+                lookback_window=upsert_input.lookback_window
+                if hasattr(upsert_input, "lookback_window")
+                else "",
             )
         return generic_config
     except (KeyError, ValidationError, AttributeError) as exc:  # pragma: no cover
+        print("exc!!!!", exc)
         raise DJInvalidInputException(  # pragma: no cover
             message=(
                 "No change has been made to the materialization config for "
