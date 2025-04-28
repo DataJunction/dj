d560c20671f4ecde039ec7b466ab0f4562c6cbde
Yian Shang <yian.shang@gmail.com>
2025-04-28 07:57:21 -0700

    lint

--- datajunction-server/datajunction_server/alembic/versions/2025_04_28_1454-3a8aea8c1862_add_missing_indexes.py
+++ datajunction-server/datajunction_server/alembic/versions/2025_04_28_1454-3a8aea8c1862_add_missing_indexes.py
@@ -5,12 +5,13 @@ Revision ID: 3a8aea8c1862
 Revises: 51547dcccb10
 Create Date: 2025-04-28 14:54:08.471638+00:00
 """
+
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '3a8aea8c1862'
-down_revision = '51547dcccb10'
+revision = "3a8aea8c1862"
+down_revision = "51547dcccb10"
 branch_labels = None
 depends_on = None
 
