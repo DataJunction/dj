73497574fa3ea44cbf9b67c7fa3a7deb3b6817ef
Yian Shang <yian.shang@gmail.com>
2025-05-24 22:51:30 -0700

    Add support to complex expressions inside distinct-agg metrics

--- datajunction-server/tests/api/sql_v2_test.py
+++ datajunction-server/tests/api/sql_v2_test.py
@@ -1176,18 +1176,9 @@ async def test_measures_sql_errors(
     ]
 
 
-@pytest.mark.asyncio
-async def test_measures_sql_agg_distinct_metric(
-    module__client_with_roads: AsyncClient,
-    duckdb_conn: duckdb.DuckDBPyConnection,
-):
-    """
-    Test `GET /sql/measures` with metrics that have an aggregation that uses the
-    DISTINCT quantifier, like COUNT(DISTINCT ...).
-    """
-    await fix_dimension_links(module__client_with_roads)
+async def create_metric_distinct_single_column(client: AsyncClient):
     metric_name = "default.number_of_hard_hats"
-    await module__client_with_roads.post(
+    await client.post(
         "/nodes/metric",
         json={
             "description": "A count distinct metric",
@@ -1196,7 +1187,7 @@ async def test_measures_sql_agg_distinct_metric(
             "name": metric_name,
         },
     )
-    response = await module__client_with_roads.get(f"/metrics/{metric_name}")
+    response = await client.get(f"/metrics/{metric_name}")
     metric_data = response.json()
     assert metric_data["measures"] == [
         {
@@ -1210,11 +1201,56 @@ async def test_measures_sql_agg_distinct_metric(
         metric_data["derived_expression"]
         == "COUNT( DISTINCT hard_hat_id_distinct_c311610d)"
     )
+    return metric_name
+
+
+async def create_metric_distinct_expression(client: AsyncClient):
+    metric_name = "default.distinct_hard_hat_id_expression"
+    await client.post(
+        "/nodes/metric",
+        json={
+            "description": "A count distinct metric",
+            "query": "SELECT COUNT(DISTINCT IF(hard_hat_id = 1, 1, 0)) FROM default.repair_orders_fact",
+            "mode": "published",
+            "name": metric_name,
+        },
+    )
+    response = await client.get(f"/metrics/{metric_name}")
+    metric_data = response.json()
+    assert metric_data["measures"] == [
+        {
+            "aggregation": None,
+            "expression": "IF(hard_hat_id = 1, 1, 0)",
+            "name": "hard_hat_id_distinct_276dd924",
+            "rule": {"level": ["IF(hard_hat_id = 1, 1, 0)"], "type": "limited"},
+        },
+    ]
+    assert (
+        metric_data["derived_expression"]
+        == "COUNT( DISTINCT hard_hat_id_distinct_276dd924)"
+    )
+    return metric_name
+
+
+@pytest.mark.asyncio
+async def test_measures_sql_agg_distinct_metric(
+    module__client_with_roads: AsyncClient,
+    duckdb_conn: duckdb.DuckDBPyConnection,
+):
+    """
+    Test `GET /sql/measures` with metrics that have an aggregation that uses the
+    DISTINCT quantifier, like COUNT(DISTINCT ...).
+    """
+    await fix_dimension_links(module__client_with_roads)
+    metric_simple = await create_metric_distinct_single_column(
+        module__client_with_roads,
+    )
+    metric_complex = await create_metric_distinct_expression(module__client_with_roads)
 
     response = await module__client_with_roads.get(
         "/sql/measures/v2",
         params={
-            "metrics": ["default.avg_repair_price", "default.number_of_hard_hats"],
+            "metrics": ["default.avg_repair_price", metric_simple, metric_complex],
             "dimensions": [
                 "default.dispatcher.company_name",
             ],
@@ -1264,30 +1300,34 @@ async def test_measures_sql_agg_distinct_metric(
       default_DOT_repair_orders_fact_built.default_DOT_dispatcher_DOT_company_name,
       COUNT(price) AS price_count_78a5eb43,
       SUM(price) AS price_sum_78a5eb43,
-      hard_hat_id AS hard_hat_id_distinct_c311610d
+      hard_hat_id AS hard_hat_id_distinct_c311610d,
+      IF(hard_hat_id = 1, 1, 0) AS hard_hat_id_distinct_276dd924
     FROM default_DOT_repair_orders_fact_built
-    GROUP BY  default_DOT_repair_orders_fact_built.default_DOT_dispatcher_DOT_company_name, default_DOT_repair_orders_fact_built.hard_hat_id
+    GROUP BY
+      default_DOT_repair_orders_fact_built.default_DOT_dispatcher_DOT_company_name,
+      hard_hat_id,
+      IF(hard_hat_id = 1, 1, 0)
     """
     assert str(parse(str(expected_sql))) == str(parse(str(translated_sql["sql"])))
     result = duckdb_conn.sql(translated_sql["sql"])
     assert set(result.fetchall()) == {
-        ("Federal Roads Group", 1, 63708.0, 1),
-        ("Pothole Pete", 1, 67253.0, 3),
-        ("Asphalts R Us", 2, 114665.0, 5),
-        ("Pothole Pete", 3, 154983.0, 1),
-        ("Asphalts R Us", 1, 76463.0, 8),
-        ("Asphalts R Us", 2, 162413.0, 3),
-        ("Asphalts R Us", 1, 63918.0, 4),
-        ("Federal Roads Group", 2, 118999.0, 5),
-        ("Federal Roads Group", 1, 27222.0, 4),
-        ("Pothole Pete", 2, 125194.0, 4),
-        ("Federal Roads Group", 1, 54901.0, 8),
-        ("Asphalts R Us", 2, 133859.0, 6),
-        ("Federal Roads Group", 2, 78603.0, 2),
-        ("Federal Roads Group", 1, 70418.0, 9),
-        ("Pothole Pete", 1, 62928.0, 6),
-        ("Federal Roads Group", 1, 53374.0, 7),
-        ("Pothole Pete", 1, 87289.0, 5),
+        ("Federal Roads Group", 1, 63708.0, 1, 1),
+        ("Pothole Pete", 1, 67253.0, 3, 0),
+        ("Asphalts R Us", 2, 114665.0, 5, 0),
+        ("Pothole Pete", 3, 154983.0, 1, 1),
+        ("Asphalts R Us", 1, 76463.0, 8, 0),
+        ("Asphalts R Us", 2, 162413.0, 3, 0),
+        ("Asphalts R Us", 1, 63918.0, 4, 0),
+        ("Federal Roads Group", 2, 118999.0, 5, 0),
+        ("Federal Roads Group", 1, 27222.0, 4, 0),
+        ("Pothole Pete", 2, 125194.0, 4, 0),
+        ("Federal Roads Group", 1, 54901.0, 8, 0),
+        ("Asphalts R Us", 2, 133859.0, 6, 0),
+        ("Federal Roads Group", 2, 78603.0, 2, 0),
+        ("Federal Roads Group", 1, 70418.0, 9, 0),
+        ("Pothole Pete", 1, 62928.0, 6, 0),
+        ("Federal Roads Group", 1, 53374.0, 7, 0),
+        ("Pothole Pete", 1, 87289.0, 5, 0),
     }
 
 
