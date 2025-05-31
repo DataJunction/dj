ed5a51c8d02b89925cac4fb41711a5ec61c47f68
Yian <yian.shang@gmail.com>
2025-05-30 22:34:42 -0700

    Add endpoint to list available dialects (#1393)
    * Add endpoint to list available dialects
    
    * Add support for GraphQL querying of dialects

--- datajunction-server/datajunction_server/api/graphql/queries/engines.py
+++ datajunction-server/datajunction_server/api/graphql/queries/engines.py
@@ -7,7 +7,8 @@ from typing import List
 from sqlalchemy import select
 from strawberry.types import Info
 
-from datajunction_server.api.graphql.scalars.catalog_engine import Engine
+from datajunction_server.models.dialect import DialectRegistry
+from datajunction_server.api.graphql.scalars.catalog_engine import Engine, DialectInfo
 from datajunction_server.database.engine import Engine as DBEngine
 
 
@@ -22,4 +23,20 @@ async def list_engines(
     return [
         Engine.from_pydantic(engine)  # type: ignore #pylint: disable=E1101
         for engine in (await session.execute(select(DBEngine))).scalars().all()
+    ]
+
+
+async def list_dialects(
+    *,
+    info: Info = None,
+) -> List[DialectInfo]:
+    """
+    List all supported dialects
+    """
+    return [
+        DialectInfo(  # type: ignore
+            name=dialect,
+            plugin_class=plugin.__name__,
+        )
+        for dialect, plugin in DialectRegistry._registry.items()
     ]
