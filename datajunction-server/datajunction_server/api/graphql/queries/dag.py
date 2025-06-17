0367542ea9c8b2093702d0258f8d73495dee486b
Olek Górajek <agorajek@users.noreply.github.com>
2025-06-16 23:42:51 -0700

    Add custom_metadata and downstream nodes to GraphQL api. (#1401)

--- datajunction-server/datajunction_server/api/graphql/queries/dag.py
+++ datajunction-server/datajunction_server/api/graphql/queries/dag.py
@@ -7,9 +7,11 @@ from typing import Annotated
 import strawberry
 from strawberry.types import Info
 
+from datajunction_server.database.node import Node
 from datajunction_server.api.graphql.resolvers.nodes import find_nodes_by
 from datajunction_server.api.graphql.scalars.node import DimensionAttribute
-from datajunction_server.sql.dag import get_common_dimensions
+from datajunction_server.sql.dag import get_common_dimensions, get_downstream_nodes
+from datajunction_server.models.node_type import NodeType
 from datajunction_server.utils import SEPARATOR
 
 
@@ -32,8 +34,31 @@ async def common_dimensions(
         DimensionAttribute(  # type: ignore
             name=dim.name,
             attribute=dim.name.split(SEPARATOR)[-1],
-            properties=dim.properties,
-            type=dim.type,
+            properties=dim.properties,  # type: ignore
+            type=dim.type,  # type: ignore
         )
         for dim in dimensions
     ]
+
+
+async def downstream_nodes(
+    node_name: Annotated[
+        str,
+        strawberry.argument(
+            description="The node name to find downstream nodes for.",
+        ),
+    ],
+    node_type: Annotated[
+        NodeType | None,
+        strawberry.argument(
+            description="The node type to filter the downstream nodes on.",
+        ),
+    ] = None,
+    *,
+    info: Info,
+) -> list[Node]:
+    """
+    Return a list of downstream nodes for a given node.
+    """
+    session = info.context["session"]
+    return await get_downstream_nodes(session, node_name=node_name, node_type=node_type)  # type: ignore
