f485cea0597791a7eff6e83c775e98bfb181b0a7
Yian <yian.shang@gmail.com>
2025-06-18 20:10:17 -0700

    Add GraphQL support for generating measures SQL directly on a cube (#1408)

--- datajunction-server/datajunction_server/api/graphql/queries/sql.py
+++ datajunction-server/datajunction_server/api/graphql/queries/sql.py
@@ -1,58 +1,19 @@
 """Generate SQL-related GraphQL queries."""
 
-from typing import Annotated, Optional, OrderedDict
+from typing import Annotated, Optional
 
 import strawberry
 from strawberry.types import Info
 
-from datajunction_server.api.graphql.scalars.sql import GeneratedSQL
+from datajunction_server.api.graphql.resolvers.nodes import (
+    resolve_metrics_and_dimensions,
+)
+from datajunction_server.api.graphql.scalars.sql import (
+    GeneratedSQL,
+    CubeDefinition,
+    EngineSettings,
+)
 from datajunction_server.construction.build_v2 import get_measures_query
-
-
-@strawberry.input
-class CubeDefinition:
-    """
-    The cube definition for the query
-    """
-
-    metrics: Annotated[
-        list[str],
-        strawberry.argument(
-            description="A list of metric node names",
-        ),
-    ] = None  # type: ignore
-    dimensions: Annotated[
-        list[str] | None,
-        strawberry.argument(
-            description="A list of dimension attribute names",
-        ),
-    ] = None
-    filters: Annotated[
-        list[str] | None,
-        strawberry.argument(
-            description="A list of filter SQL clauses",
-        ),
-    ] = None
-    orderby: Annotated[
-        list[str] | None,
-        strawberry.argument(
-            description="A list of order by clauses",
-        ),
-    ] = None
-
-
-@strawberry.input
-class EngineSettings:
-    """
-    The engine settings for the query
-    """
-
-    name: str = strawberry.field(
-        description="The name of the engine used by the generated SQL",
-    )
-    version: str | None = strawberry.field(
-        description="The version of the engine used by the generated SQL",
-    )
 
 
 async def measures_sql(
@@ -85,10 +46,11 @@ async def measures_sql(
     Get measures SQL for a set of metrics with dimensions and filters
     """
     session = info.context["session"]
+    metrics, dimensions = await resolve_metrics_and_dimensions(session, cube)
     queries = await get_measures_query(
         session=session,
-        metrics=list(OrderedDict.fromkeys(cube.metrics)),  # type: ignore
-        dimensions=cube.dimensions,  # type: ignore
+        metrics=metrics,  # type: ignore
+        dimensions=dimensions,  # type: ignore
         filters=cube.filters,  # type: ignore
         orderby=cube.orderby,
         engine_name=engine.name if engine else None,
