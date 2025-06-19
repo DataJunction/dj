be72233481832ffbb689a6cc9ec4b8f5dc38a560
Yian Shang <yian.shang@gmail.com>
2025-06-18 17:42:13 -0700

    clean up

--- datajunction-server/datajunction_server/api/graphql/queries/sql.py
+++ datajunction-server/datajunction_server/api/graphql/queries/sql.py
@@ -11,6 +11,7 @@ from datajunction_server.database.node import Node as DBNode
 from datajunction_server.api.graphql.resolvers.nodes import find_nodes_by
 from datajunction_server.api.graphql.scalars.sql import GeneratedSQL
 from datajunction_server.construction.build_v2 import get_measures_query
+from datajunction_server.utils import dedupe_append
 
 
 @strawberry.input
@@ -96,15 +97,13 @@ async def measures_sql(
     session = info.context["session"]
     metrics = cube.metrics or []
     dimensions = cube.dimensions or []
+
     if cube.cube:
         cube_node = await DBNode.get_cube_by_name(session, cube.cube)
         if not cube_node:
             raise DJNodeNotFound(f"Cube '{cube.cube}' not found.")
-        current_revision = cube_node.current
-        cube_node_metrics = set(current_revision.cube_node_metrics)
-        cube_node_dimensions = set(current_revision.cube_node_dimensions)
-        metrics = current_revision.cube_node_metrics + [m for m in metrics if m not in cube_node_metrics]
-        dimensions = current_revision.cube_node_dimensions + [dim for dim in dimensions if dim not in cube_node_dimensions]
+        metrics = dedupe_append(cube_node.current.cube_node_metrics, metrics)
+        dimensions = dedupe_append(cube_node.current.cube_node_dimensions, dimensions)
 
     queries = await get_measures_query(
         session=session,
