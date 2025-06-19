45fdafa1bffbd209dc335f44d5723b2ae4f0db80
Yian Shang <yian.shang@gmail.com>
2025-06-18 18:49:03 -0700

    Add filtering on columns for node revisions in GQL

--- datajunction-server/tests/api/graphql/find_nodes_test.py
+++ datajunction-server/tests/api/graphql/find_nodes_test.py
@@ -1101,3 +1101,133 @@ async def test_find_nodes_paginated_empty_list(
             "hasPrevPage": False,
         },
     }
+
+
+def test_columns_resolver_filters_by_attribute():
+    """
+    Test that the columns resolver filters columns by attribute
+    """
+    mock_column = mock.MagicMock()
+    from datajunction_server.api.graphql.scalars.node import NodeRevision
+    from datajunction_server.database import (
+        NodeRevision as DBNodeRevision,
+        ColumnAttribute,
+        AttributeType,
+    )
+    from datajunction_server.database.column import Column
+
+    import datajunction_server.sql.parsing.types as ct
+    from datajunction_server.models.node_type import NodeType
+
+    primary_key_attribute = AttributeType(namespace="system", name="primary_key")
+    dimension_attribute = AttributeType(namespace="system", name="dimension")
+    db_node_revision = DBNodeRevision(
+        name="source_rev",
+        type=NodeType.SOURCE,
+        version="1",
+        columns=[
+            Column(
+                name="random_primary_key",
+                type=ct.StringType(),
+                attributes=[ColumnAttribute(attribute_type=primary_key_attribute)],
+                order=0,
+            ),
+            Column(
+                name="user_id",
+                type=ct.IntegerType(),
+                attributes=[ColumnAttribute(attribute_type=dimension_attribute)],
+                order=2,
+            ),
+            Column(
+                name="foo",
+                type=ct.FloatType(),
+                order=3,
+            ),
+        ],
+    )
+
+    mock_node = mock.MagicMock()
+    mock_node.columns = [mock_column]
+
+    result = NodeRevision.columns(
+        NodeRevision,
+        root=db_node_revision,
+        attributes=["primary_key"],
+    )
+    assert len(result) == 1
+    assert result[0].name == "random_primary_key"
+
+    result = NodeRevision.columns(
+        NodeRevision,
+        root=db_node_revision,
+        attributes=["dimension"],
+    )
+    assert len(result) == 1
+    assert result[0].name == "user_id"
+
+
+@pytest.mark.asyncio
+async def test_find_by_with_filtering_on_columns(
+    module__client_with_roads: AsyncClient,
+) -> None:
+    """
+    Test that filter on columns works correctly
+    """
+    query = """
+    {
+        findNodes(names: ["default.regional_level_agg", "default.repair_orders"]) {
+            name
+            type
+            current {
+                columns(attributes: ["primary_key"]) {
+                    name
+                    type
+                }
+            }
+            currentVersion
+        }
+    }
+    """
+
+    response = await module__client_with_roads.post("/graphql", json={"query": query})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["data"]["findNodes"] == [
+        {
+            "current": {
+                "columns": [
+                    {
+                        "name": "us_region_id",
+                        "type": "int",
+                    },
+                    {
+                        "name": "state_name",
+                        "type": "string",
+                    },
+                    {
+                        "name": "order_year",
+                        "type": "int",
+                    },
+                    {
+                        "name": "order_month",
+                        "type": "int",
+                    },
+                    {
+                        "name": "order_day",
+                        "type": "int",
+                    },
+                ],
+            },
+            "currentVersion": "v1.0",
+            "name": "default.regional_level_agg",
+            "type": "TRANSFORM",
+        },
+        {
+            "current": {
+                "columns": [],
+            },
+            "currentVersion": "v1.0",
+            "name": "default.repair_orders",
+            "type": "SOURCE",
+        },
+    ]
