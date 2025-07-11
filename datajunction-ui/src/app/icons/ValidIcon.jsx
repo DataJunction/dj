330053bfaf56eb04f9a0ce26251410cf0c78c400
Yian Shang <yian.shang@gmail.com>
2025-07-10 23:51:38 -0700

    Fix overview dash

--- datajunction-ui/src/app/icons/ValidIcon.jsx
+++ datajunction-ui/src/app/icons/ValidIcon.jsx
@@ -1,8 +1,9 @@
-const ValidIcon = ({ width = '25px', height = '25px' }) => (
+const ValidIcon = ({ width = '25px', height = '25px', style = {} }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
     height={height}
+    style={style}
     fill="currentColor"
     className="bi bi-check-circle-fill"
     viewBox="0 0 16 16"
--- datajunction-ui/src/app/pages/OverviewPage/index.jsx
+++ datajunction-ui/src/app/pages/OverviewPage/index.jsx
@@ -1,26 +1,20 @@
-import * as React from 'react';
-import { useParams } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import DJClientContext from '../../providers/djclient';
-import { OverviewChart } from './OverviewChart';
+import { OverviewPanel } from './OverviewPanel';
 import { NodesByTypeChart } from './NodesByTypeChart';
-import { ByStatusChart } from './ByStatusChart';
+import { ByStatusPanel } from './ByStatusPanel';
 import { WithoutDescriptionPanel } from './WithoutDescriptionPanel';
 import { TrendsPanel } from './TrendsPanel';
 import {
-  PieChart,
-  Pie,
   Legend,
   Tooltip,
   ResponsiveContainer,
   BarChart,
-  CartesianGrid,
   XAxis,
   YAxis,
   Bar,
-  Cell,
 } from 'recharts';
-import InvalidIcon from 'app/icons/InvalidIcon';
+import { NodeMetadataPanel } from './NodeMetadataPanel';
 
 export function OverviewPage() {
   const djClient = useContext(DJClientContext).DataJunctionAPI;
@@ -30,7 +24,6 @@ export function OverviewPage() {
 
   useEffect(() => {
     const fetchData = async () => {
-      // setNodeTrends(await djClient.analytics.node_trends());
       setNodesByUser(await djClient.analytics.node_counts_by_user());
       setDimensionNodes(await djClient.analytics.dimensions());
     };
@@ -50,90 +43,48 @@ export function OverviewPage() {
   return (
     <div className="mid">
       <div className="chart-container">
-        <OverviewChart />
+        <OverviewPanel />
         <NodesByTypeChart />
-        <TrendsPanel />
-      </div>
-
-      <div className="chart-container">
-        <ByStatusChart />
+        {/* <ByStatusPanel /> */}
         <WithoutDescriptionPanel />
       </div>
 
       <div className="chart-container">
+        <TrendsPanel />
+      </div>
+
+      <div className="chart-container">
         <div className="chart-box">
-          <div className="chart-title">Top 15 Node Creators</div>
-          {console.log('nodesByUser', nodesByUser)}
-          {nodesByUser ? (
-            <ResponsiveContainer width="100%" height={400}>
-              <BarChart
-                layout="vertical"
-                width={1000}
-                height={400}
-                data={nodesByUser.slice(0, 15)}
-                margin={{
-                  top: 20,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <YAxis dataKey="username" type="category" />
-                <XAxis type="number" />
-                <Tooltip />
-                <Legend />
-                {Object.entries(COLOR_MAPPING).map(([key, color]) => (
-                  <Bar
-                    key={key}
-                    dataKey={key}
-                    stackId="nodeCount"
-                    fill={color}
-                    name={key}
-                  />
-                ))}
-              </BarChart>
-            </ResponsiveContainer>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className="chart-box">
-          <div className="chart-title">Links per Dimension Node</div>
+          <div className="chart-title">Dimension Node Usage</div>
           <table className="card-inner-table table">
             <thead className="fs-7 fw-bold text-gray-400 border-bottom-0">
               <tr>
                 <th className="text-start">Dimension</th>
-                <th className="a">Link Count</th>
+                <th className="a">Links</th>
+                <th className="a">Cubes</th>
               </tr>
             </thead>
             <tbody>
+              {console.log('dimnodes', dimensionNodes)}
               {dimensionNodes
-                ?.map(dim => {
-                  return {
-                    name: dim.name,
-                    value: dim.indegree,
-                  };
-                })
-                .sort((a, b) => b.value - a.value) // assuming `value` is indegree
+                ?.sort(
+                  (a, b) =>
+                    b.cube_count + b.indegree - (a.cube_count + a.indegree),
+                )
                 .slice(0, 10)
                 .map((dim, index) => (
                   <tr key={index}>
-                    <td className="a">{dim.name}</td>
-                    <td className="a">{dim.value}</td>
+                    <td className="a">
+                      <a href={`/nodes/${dim.name}`}>{dim.name}</a>
+                    </td>
+                    <td className="a">{dim.indegree}</td>
+                    <td className="a">{dim.cube_count}</td>
                   </tr>
                 ))}
             </tbody>
           </table>
         </div>
-        <div className="chart-box">
-          <div className="chart-title">Orphaned Dimension Nodes</div>
-          {
-            dimensionNodes?.filter(
-              dim => dim.indegree === 0 || dim.cube_count === 0,
-            ).length
-          }
-        </div>
+        <NodeMetadataPanel />
       </div>
     </div>
   );
