fe4c8bd8501a3b6aeff908cb852dbfde241201be
Yian Shang <yian.shang@gmail.com>
2025-07-13 01:27:12 -0700

    Fix

--- datajunction-ui/src/app/pages/OverviewPage/NodesByTypePanel.jsx
+++ datajunction-ui/src/app/pages/OverviewPage/NodesByTypePanel.jsx
@@ -16,28 +16,46 @@ const COLOR_MAPPING = {
 export const NodesByTypePanel = () => {
   const djClient = useContext(DJClientContext).DataJunctionAPI;
   const [nodesByType, setNodesByType] = useState(null);
+  const [materializationsByType, setMaterializationsByType] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       setNodesByType(await djClient.analytics.node_counts_by_type());
+      setMaterializationsByType(await djClient.analytics.materialization_counts_by_type());
     };
     fetchData().catch(console.error);
   }, [djClient]);
 
   return (
-    <div className="chart-box" style={{ flex: '0 0 30%', maxWidth: '350px' }}>
-      <div className="chart-title">Nodes by Type</div>
-      <div className="horiz-box">
-        {nodesByType?.map(entry => (
-          <div className="vert-box">
-            <NodeIcon color={COLOR_MAPPING[entry.name]} />
-            <strong style={{ color: COLOR_MAPPING[entry.name] }}>
-              {entry.value}
-            </strong>
-            <span>{entry.name}s</span>
-          </div>
-        ))}
+    <>
+      <div className="chart-box" style={{ flex: '0 0 30%', maxWidth: '350px' }}>
+        <div className="chart-title">Nodes by Type</div>
+        <div className="horiz-box">
+          {nodesByType?.map(entry => (
+            <div className="vert-box">
+              <NodeIcon color={COLOR_MAPPING[entry.name]} />
+              <strong style={{ color: COLOR_MAPPING[entry.name] }}>
+                {entry.value}
+              </strong>
+              <span>{entry.name}s</span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="chart-box" style={{ flex: '0 0 30%', maxWidth: '350px' }}>
+        <div className="chart-title">Materializations by Type</div>
+        <div className="horiz-box">
+          {materializationsByType?.map(entry => (
+            <div className="vert-box">
+              <NodeIcon color={COLOR_MAPPING[entry.name]} />
+              <strong style={{ color: COLOR_MAPPING[entry.name] }}>
+                {entry.value}
+              </strong>
+              <span>{entry.name}s</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
