e32469a31884d6687f64164d1c6c3f789314064b
Yian Shang <yian.shang@gmail.com>
2025-07-17 00:39:41 -0700

    Add dj cli support for seeding system nodes

--- datajunction-server/datajunction_server/api/main.py
+++ datajunction-server/datajunction_server/api/main.py
@@ -72,91 +72,97 @@ async def lifespan(app: FastAPI):  # pragma: no cover
     yield
 
 
-app = FastAPI(
-    title=settings.name,
-    description=settings.description,
-    version=__version__,
-    license_info={
-        "name": "MIT License",
-        "url": "https://mit-license.org/",
-    },
-    lifespan=lifespan,
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.cors_origin_whitelist,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.include_router(catalogs.router)
-app.include_router(collection.router)
-app.include_router(engines.router)
-app.include_router(metrics.router)
-app.include_router(djsql.router)
-app.include_router(nodes.router)
-app.include_router(namespaces.router)
-app.include_router(materializations.router)
-app.include_router(measures.router)
-app.include_router(data.router)
-app.include_router(health.router)
-app.include_router(history.router)
-app.include_router(cubes.router)
-app.include_router(tags.router)
-app.include_router(attributes.router)
-app.include_router(sql.router)
-app.include_router(client.router)
-app.include_router(dimensions.router)
-app.include_router(graphql_app, prefix="/graphql")
-app.include_router(whoami.router)
-app.include_router(users.router)
-app.include_router(basic.router)
-app.include_router(notifications.router)
-
-
-@app.exception_handler(DJException)
-async def dj_exception_handler(
-    request: Request,
-    exc: DJException,
-) -> JSONResponse:
-    """
-    Capture errors and return JSON.
-    """
-    _logger.exception(exc)
-    response = JSONResponse(
-        status_code=exc.http_status_code,
-        content=exc.to_dict(),
-        headers={"X-DJ-Error": "true", "X-DBAPI-Exception": exc.dbapi_exception},
+def create_app(lifespan):
+    app = FastAPI(
+        title=settings.name,
+        description=settings.description,
+        version=__version__,
+        license_info={
+            "name": "MIT License",
+            "url": "https://mit-license.org/",
+        },
+        lifespan=lifespan,
     )
-    # If unauthorized, clear out any DJ cookies
-    if exc.http_status_code == HTTPStatus.UNAUTHORIZED:
-        response.delete_cookie(AUTH_COOKIE, httponly=True)
-        response.delete_cookie(LOGGED_IN_FLAG_COOKIE)
-    return response
+    configure_app(app)
+    return app
 
 
-# Only mount github auth router if a github client id and secret are configured
-if all(
-    [
-        settings.secret,
-        settings.github_oauth_client_id,
-        settings.github_oauth_client_secret,
-    ],
-):  # pragma: no cover
-    from datajunction_server.api.access.authentication import github
+def configure_app(app: FastAPI) -> None:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.cors_origin_whitelist,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.include_router(catalogs.router)
+    app.include_router(collection.router)
+    app.include_router(engines.router)
+    app.include_router(metrics.router)
+    app.include_router(djsql.router)
+    app.include_router(nodes.router)
+    app.include_router(namespaces.router)
+    app.include_router(materializations.router)
+    app.include_router(measures.router)
+    app.include_router(data.router)
+    app.include_router(health.router)
+    app.include_router(history.router)
+    app.include_router(cubes.router)
+    app.include_router(tags.router)
+    app.include_router(attributes.router)
+    app.include_router(sql.router)
+    app.include_router(client.router)
+    app.include_router(dimensions.router)
+    app.include_router(graphql_app, prefix="/graphql")
+    app.include_router(whoami.router)
+    app.include_router(users.router)
+    app.include_router(basic.router)
+    app.include_router(notifications.router)
 
-    app.include_router(github.router)
+    @app.exception_handler(DJException)
+    async def dj_exception_handler(
+        request: Request,
+        exc: DJException,
+    ) -> JSONResponse:
+        """
+        Capture errors and return JSON.
+        """
+        _logger.exception(exc)
+        response = JSONResponse(
+            status_code=exc.http_status_code,
+            content=exc.to_dict(),
+            headers={"X-DJ-Error": "true", "X-DBAPI-Exception": exc.dbapi_exception},
+        )
+        # If unauthorized, clear out any DJ cookies
+        if exc.http_status_code == HTTPStatus.UNAUTHORIZED:
+            response.delete_cookie(AUTH_COOKIE, httponly=True)
+            response.delete_cookie(LOGGED_IN_FLAG_COOKIE)
+        return response
 
-# Only mount google auth router if a google oauth is configured
-if all(
-    [
-        settings.secret,
-        settings.google_oauth_client_id,
-        settings.google_oauth_client_secret,
-        settings.google_oauth_client_secret_file,
-    ],
-):  # pragma: no cover
-    from datajunction_server.api.access.authentication import google
+    # Only mount github auth router if a github client id and secret are configured
+    if all(
+        [
+            settings.secret,
+            settings.github_oauth_client_id,
+            settings.github_oauth_client_secret,
+        ],
+    ):  # pragma: no cover
+        from datajunction_server.api.access.authentication import github
 
-    app.include_router(google.router)
+        app.include_router(github.router)
+
+    # Only mount google auth router if a google oauth is configured
+    if all(
+        [
+            settings.secret,
+            settings.google_oauth_client_id,
+            settings.google_oauth_client_secret,
+            settings.google_oauth_client_secret_file,
+        ],
+    ):  # pragma: no cover
+        from datajunction_server.api.access.authentication import google
+
+        app.include_router(google.router)
+
+
+app = create_app(lifespan=lifespan)
