8c91bfd9a0cb1350228b5f7e88b11eb60b0b72ef
Yian Shang <yian.shang@gmail.com>
2025-09-11 23:52:04 -0700

    Speed up the hard deletion of namespaces, along with cascading hard deletion of nodes

--- datajunction-server/datajunction_server/internal/namespaces.py
+++ datajunction-server/datajunction_server/internal/namespaces.py
@@ -2,13 +2,14 @@
 Helper methods for namespaces endpoints.
 """
 
+from collections import defaultdict
 import logging
 import os
 import re
 from datetime import datetime, timezone
 from typing import Callable, Dict, List, Tuple
 
-from sqlalchemy import or_, select
+from sqlalchemy import or_, select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
@@ -25,11 +26,10 @@ from datajunction_server.errors import (
 from datajunction_server.internal.history import ActivityType, EntityType
 from datajunction_server.internal.nodes import (
     get_single_cube_revision_metadata,
-    hard_delete_node,
 )
 from datajunction_server.models.node import NodeMinimumDetail
 from datajunction_server.models.node_type import NodeType
-from datajunction_server.sql.dag import topological_sort
+from datajunction_server.sql.dag import get_downstream_nodes, topological_sort
 from datajunction_server.typing import UTCDatetime
 from datajunction_server.utils import SEPARATOR
 
@@ -276,25 +276,74 @@ async def hard_delete_namespace(
                 " this action cannot be undone."
             ),
         )
+    impacts = {
+        node_name: {"type": "node in namespace", "status": "hard deleted"}
+        for node_name in node_names
+    }
 
-    impacts = {}
-    for node_name in node_names:
-        impacts[node_name] = await hard_delete_node(
-            node_name,
-            session,
-            current_user=current_user,
-            save_history=save_history,
-        )
+    # Track downstream nodes affected by deletions
+    impacted_downstreams = defaultdict(list)
+    for name in node_names:
+        for downstream_node in await get_downstream_nodes(
+            session=session,
+            node_name=name,
+        ):
+            if downstream_node.name not in node_names:
+                impacted_downstreams[downstream_node.name].append(name)
 
+    # Save history and update impacts for downstream nodes
+    for impacted_node, causes in impacted_downstreams.items():
+        for name in causes:
+            await save_history(
+                event=History(
+                    entity_type=EntityType.DEPENDENCY,
+                    entity_name=name,
+                    node=impacted_node,
+                    activity_type=ActivityType.DELETE,
+                    user=current_user.username,
+                ),
+                session=session,
+            )
+        impacts[impacted_node] = {
+            "type": "downstream",
+            "status": "invalid",
+            "caused_by": causes,  # type: ignore
+        }
+
+    # Delete the nodes
+    await session.execute(delete(Node).where(Node.name.in_(node_names)))
+    await session.commit()
+
+    # Delete namespaces and record impact
     namespaces = await list_namespaces_in_hierarchy(session, namespace)
     for _namespace in namespaces:
         impacts[_namespace.namespace] = {
-            "namespace": _namespace.namespace,
-            "status": "deleted",
+            "type": "namespace",
+            "status": "hard deleted",
         }
         await session.delete(_namespace)
     await session.commit()
-    return impacts
+
+    response = {
+        "nodes_in_namespace": [
+            {"name": n, "status": impacts[n]["status"]} for n in node_names
+        ],
+        "downstream_nodes": [
+            {
+                "name": n,
+                "status": impacts[n]["status"],
+                "caused_by": impacts[n]["caused_by"],
+            }
+            for n in impacted_downstreams
+        ],
+        "namespaces": [
+            {"name": n, "status": impacts[n]["status"]}
+            for n, data in impacts.items()
+            if data["type"] == "namespace"
+        ],
+    }
+
+    return response
 
 
 def _get_dir_and_filename(
