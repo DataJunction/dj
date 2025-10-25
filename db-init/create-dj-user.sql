daa369056b832cc0a29f34981ab23b5b884a2a48
samredai <samuelspersonalemail@gmail.com>
2025-10-25 12:36:42 -0400

    include principalkind in when creating dj user in demo setup

--- db-init/create-dj-user.sql
+++ db-init/create-dj-user.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, oauth_provider, is_admin)
+INSERT INTO users (username, password, oauth_provider, is_admin, kind)
 VALUES
-    ('dj', '$2b$12$K5oXl1Qs/UiNzvysOckn2uJjJmGHrhnk97hFRlMboP4NbvNbtoQ4a', 'BASIC', false)
+    ('dj', '$2b$12$K5oXl1Qs/UiNzvysOckn2uJjJmGHrhnk97hFRlMboP4NbvNbtoQ4a', 'BASIC', false, 'USER')
 ON CONFLICT (username)
 DO NOTHING;
