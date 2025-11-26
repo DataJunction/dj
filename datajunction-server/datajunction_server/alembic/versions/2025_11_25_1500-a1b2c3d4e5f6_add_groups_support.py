49145542b290f0c211a8acdae4a7e434149f2174
Yian Shang <yian.shang@gmail.com>
2025-11-25 18:57:07 -0800

    Fix database migration

--- datajunction-server/datajunction_server/alembic/versions/2025_11_25_1500-a1b2c3d4e5f6_add_groups_support.py
+++ datajunction-server/datajunction_server/alembic/versions/2025_11_25_1500-a1b2c3d4e5f6_add_groups_support.py
@@ -3,7 +3,7 @@ Add groups support
 
 Revision ID: a1b2c3d4e5f6
 Revises: be76e22dd71a
-Create Date: 2024-11-25 15:00:00.000000+00:00
+Create Date: 2025-11-25 15:00:00.000000+00:00
 """
 # pylint: disable=no-member, invalid-name, missing-function-docstring, unused-import, no-name-in-module
 
@@ -13,19 +13,14 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "a1b2c3d4e5f6"
-down_revision = "be76e22dd71a"
+down_revision = "95732205ad12"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    # 1. Add GROUP to PrincipalKind enum
-    # Note: Postgres doesn't support adding enum values in a transaction,
-    # so we use ALTER TYPE directly
-    with op.get_context().autocommit_block():
-        op.execute("ALTER TYPE principalkind ADD VALUE IF NOT EXISTS 'GROUP'")
+    op.execute("ALTER TYPE principalkind ADD VALUE 'GROUP'")
 
-    # 2. Create group_members table
     op.create_table(
         "group_members",
         sa.Column(
@@ -63,7 +58,6 @@ def upgrade():
         ),
     )
 
-    # 3. Create indexes for better query performance
     op.create_index(
         "idx_group_members_group_id",
         "group_members",
@@ -77,13 +71,11 @@ def upgrade():
 
 
 def downgrade():
-    # Drop indexes
     op.drop_index("idx_group_members_member_id", table_name="group_members")
     op.drop_index("idx_group_members_group_id", table_name="group_members")
 
-    # Drop group_members table
     op.drop_table("group_members")
 
-    # Note: Postgres doesn't support removing enum values easily
+    # Postgres doesn't support removing enum values easily
     # Users will need to manually handle enum cleanup if needed
     # Or recreate the enum without GROUP value
