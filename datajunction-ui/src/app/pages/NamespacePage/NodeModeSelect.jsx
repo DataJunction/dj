8213b795e963841ace5dc83023dc9ded6256ff1e
Yian <yian.shang@gmail.com>
2025-12-02 18:08:02 -0800

    Add Node Mode (Draft / Published) Filter to DJ UI (#1581)
    * Add the ability to filter nodes by mode (draft, published) via GraphQL
    
    * Add filtering option to UI
    
    * Add column for displaying mode
    
    * Add tests for GraphQL mode filtering
    
    * Fix tests for UI namespace loading
    
    * Add NodeModeSelect

--- datajunction-ui/src/app/pages/NamespacePage/NodeModeSelect.jsx
+++ datajunction-ui/src/app/pages/NamespacePage/NodeModeSelect.jsx
@@ -1,28 +1,25 @@
 import Select from 'react-select';
 import Control from './FieldControl';
 
-export default function NodeTypeSelect({ onChange }) {
+export default function NodeModeSelect({ onChange }) {
   return (
     <span
       className="menu-link"
       style={{ marginLeft: '30px', width: '300px' }}
-      data-testid="select-node-type"
+      data-testid="select-node-mode"
     >
       <Select
-        name="node_type"
+        name="node_mode"
         isClearable
-        label="Type"
+        label="Mode"
         components={{ Control }}
         onChange={e => onChange(e)}
         styles={{
           control: styles => ({ ...styles, backgroundColor: 'white' }),
         }}
         options={[
-          { value: 'source', label: 'Source' },
-          { value: 'transform', label: 'Transform' },
-          { value: 'dimension', label: 'Dimension' },
-          { value: 'metric', label: 'Metric' },
-          { value: 'cube', label: 'Cube' },
+          { value: 'published', label: 'Published' },
+          { value: 'draft', label: 'Draft' },
         ]}
       />
     </span>
