82bfea1151917c5e9e85189650f8d61197f9ef55
Yian Shang <yian.shang@gmail.com>
2026-02-24 13:50:51 -0800

    Fix catalog None check in dimension link validation

--- datajunction-server/datajunction_server/internal/nodes.py
+++ datajunction-server/datajunction_server/internal/nodes.py
@@ -2161,8 +2161,10 @@ async def validate_complex_dimension_link(
         )
 
     if (
-        dimension_node.current.catalog.name != settings.seed_setup.virtual_catalog_name  # type: ignore
-        and dimension_node.current.catalog is not None  # type: ignore
+        dimension_node.current.catalog is not None  # type: ignore
+        and node.current.catalog is not None  # type: ignore
+        and dimension_node.current.catalog.name
+        != settings.seed_setup.virtual_catalog_name  # type: ignore
         and node.current.catalog.name != dimension_node.current.catalog.name  # type: ignore
     ):
         raise DJInvalidInputException(  # pragma: no cover
