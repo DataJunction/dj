5726840c59d555b2388b548830fca9aac482cf46
Yian Shang <yian.shang@gmail.com>
2026-03-13 13:41:34 -0700

    Fix

--- datajunction-server/datajunction_server/alembic/versions/2026_03_12_1533-93928e3ec100_add_index_on_column_node_revision_id.py
+++ datajunction-server/datajunction_server/alembic/versions/2026_03_12_1533-93928e3ec100_add_index_on_column_node_revision_id.py
@@ -10,17 +10,21 @@ Create Date: 2026-03-12 15:33:24.910204+00:00
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '93928e3ec100'
-down_revision = 'f6a7b8c9d0e1'
+revision = "93928e3ec100"
+down_revision = "f6a7b8c9d0e1"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table('column', schema=None) as batch_op:
-        batch_op.create_index(batch_op.f('ix_column_node_revision_id'), ['node_revision_id'], unique=False)
+    with op.batch_alter_table("column", schema=None) as batch_op:
+        batch_op.create_index(
+            batch_op.f("ix_column_node_revision_id"),
+            ["node_revision_id"],
+            unique=False,
+        )
 
 
 def downgrade():
-    with op.batch_alter_table('column', schema=None) as batch_op:
-        batch_op.drop_index(batch_op.f('ix_column_node_revision_id'))
+    with op.batch_alter_table("column", schema=None) as batch_op:
+        batch_op.drop_index(batch_op.f("ix_column_node_revision_id"))
