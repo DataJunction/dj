893c834bf81e4b097389424dc0d49d8240010ad3
Yian <yian.shang@gmail.com>
2026-03-15 11:09:39 -0700

    Improve Explore chart visualization and search UX (#1883)
    * Fix headings + unify search styling
    
    * Change styling of results page and add preview to node info tab
    
    * Fix lint
    
    * Display the charts more efficiently
    
    * Fix tests

--- datajunction-ui/src/app/components/Search.jsx
+++ datajunction-ui/src/app/components/Search.jsx
@@ -10,6 +10,7 @@ export default function Search() {
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const hasLoadedRef = useRef(false);
+  const inputRef = useRef(null);
 
   const djClient = useContext(DJClientContext).DataJunctionAPI;
 
@@ -64,39 +65,39 @@ export default function Search() {
 
   return (
     <div>
-      <form
-        className="search-box"
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
+      <div className="nav-search-box" onClick={() => inputRef.current?.focus()}>
         <input
+          ref={inputRef}
           type="text"
-          placeholder={isLoading ? 'Loading...' : 'Search'}
+          placeholder={isLoading ? 'Loading...' : 'Search nodes...'}
           name="search"
           value={searchValue}
           onChange={handleChange}
           onFocus={loadSearchData}
         />
-      </form>
-      <div className="search-results">
-        {searchResults.slice(0, 20).map(item => {
-          const itemUrl =
-            item.type !== 'tag' ? `/nodes/${item.name}` : `/tags/${item.name}`;
-          return (
-            <a key={item.name} href={itemUrl}>
-              <div className="search-result-item">
-                <span className={`node_type__${item.type} badge node_type`}>
-                  {item.type}
-                </span>
-                {item.display_name} (<b>{item.name}</b>){' '}
-                {item.description ? '- ' : ' '}
-                {truncate(item.description || '')}
-              </div>
-            </a>
-          );
-        })}
       </div>
+      {searchResults.length > 0 && (
+        <div className="search-results">
+          {searchResults.slice(0, 20).map(item => {
+            const itemUrl =
+              item.type !== 'tag'
+                ? `/nodes/${item.name}`
+                : `/tags/${item.name}`;
+            return (
+              <a key={item.name} href={itemUrl}>
+                <div className="search-result-item">
+                  <span className={`node_type__${item.type} badge node_type`}>
+                    {item.type}
+                  </span>
+                  {item.display_name} (<b>{item.name}</b>){' '}
+                  {item.description ? '- ' : ' '}
+                  {truncate(item.description || '')}
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
