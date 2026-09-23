1f224592ae701d583f65dabece9c60214d147aaa
Beto Dealmeida <bdealmeida@netflix.com>
2026-09-23 11:02:46 -0300

    Update reaggregation migration head

--- datajunction-server/datajunction_server/alembic/versions/2026_08_24_0000-rg0001reaggregate_add_reaggregate_to_noderevision.py
+++ datajunction-server/datajunction_server/alembic/versions/2026_08_24_0000-rg0001reaggregate_add_reaggregate_to_noderevision.py
@@ -2,7 +2,7 @@
 Add reaggregate column to noderevision
 
 Revision ID: rg0001reaggregate
-Revises: cm0003dropowner
+Revises: ck0001results
 Create Date: 2026-08-24 00:00:00.000000+00:00
 """
 
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "rg0001reaggregate"
-down_revision = "tt0001claims"
+down_revision = "ck0001results"
 branch_labels = None
 depends_on = None
 
